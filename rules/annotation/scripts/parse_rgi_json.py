b142064bfca688932d735c565f12280bbd8b54de
Cashalow <sachalau@gmail.com>
2017-12-05 17:29:20 +0100

    adding files for managing db for resistance

--- rules/annotation/scripts/parse_rgi_json.py
+++ rules/annotation/scripts/parse_rgi_json.py
@@ -5,8 +5,7 @@ cnx = mysql.connector.connect(option_files=snakemake.input[1], option_groups="my
 cnx.get_warnings = True
 cursor = cnx.cursor()
 
-def parse_rgi_json(filename, curs, log):
-    data={}
+def parse_rgi_json(filename, curs, log, sample):
     with open(filename, "r") as f:
         rgi = json.load(f)
         for j in rgi.keys():
@@ -15,28 +14,34 @@ def parse_rgi_json(filename, curs, log):
                     try: 
                         snp=rgi[j][k]["SNP"]
                         try:
-                            antibio=rgi[j][k]["ARO_name"].split()[-1]
-                            if antibio not in data:
-                                data[antibio]={}
+                            antibio=rgi[j][k]["ARO_name"].split()[-1].strip()
                             gene = rgi[j][k]["ARO_name"].split()[2]
                             pos = snp["position"]
                             ref = snp["original"]
                             mut = snp["change"]
-                            cmd="INSERT IGNORE INTO mutations (Specimen, software, gene, position, ref_aa, mut_aa) VALUES (\"{0}\", \"rgi\", \"{1}\", {2}, \"{3}\", \"{4}\");".format(str(snakemake.wildcards.sample), gene, pos, ref, mut)
+                            cmd="INSERT IGNORE INTO mutations (Specimen, software, gene, position, ref_aa, mut_aa) VALUES (\"{0}\", \"rgi\", \"{1}\", {2}, \"{3}\", \"{4}\");".format(str(sample), gene, pos, ref, mut)
                             curs.execute(cmd)
                             i=curs.fetchwarnings()
                             if i is not None:
                                 with open(log, "a") as f:
                                     f.write(i[2]+"\n")
+                            cmd="INSERT IGNORE INTO phenotype_prediction (Specimen, software, antibiotic) VALUES (\"{0}\", \"rgi\", \"{1}\");".format(str(sample), str(antibio))
+                            curs.execute(cmd)
+                            i=curs.fetchwarnings()
+                            if i is not None:
+                                with open(log, "a") as f:
+                                    f.write(i[2]+"\n")
+
                         except IndexError:
                             pass
                     except KeyError:
                         pass
-    return(data)
+    return("ok")
+
 
 with open(snakemake.output[0], "w") as myfile:
     myfile.write("")
-    
-parse_rgi_json(snakemake.input[0], cursor, snakemake.output[0])
+
+parse_rgi_json(snakemake.input[0], cursor, snakemake.output[0], snakemake.wildcards.sample)
 cnx.commit()
 cnx.close()
