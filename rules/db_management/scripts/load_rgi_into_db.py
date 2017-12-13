906ec6b283012c4ac155d77a34ebf438697874da
Cashalow <sachalau@gmail.com>
2017-12-13 11:40:32 +0100

    adding handling of more than one mutations confering resistance in the same gene

--- rules/db_management/scripts/load_rgi_into_db.py
+++ rules/db_management/scripts/load_rgi_into_db.py
@@ -21,27 +21,30 @@ def load_row_into_db(row, curs, log, sample, aro_ont, gene_list):
     cmds = []
     if row["SNP"] != "n/a":
         best_hit = row["Best_Hit_ARO"].split()
-        gene = list(set(best_hit).intersection(set(gene_list)))
-        pos = re.sub("[^0-9]", "", row["SNP"])
-        ref = row["SNP"][0]
-        mut = row["SNP"][-1]
-        if len(gene) == 1:
-            term = aro_ont[row["ARO"]]
-            for child in term.relations:
-                if child.obo_name=="confers_resistance_to_drug" or  child.obo_name=="confers_resistance_to":
-                    for antibiotic in term.relations[child]:
-                        anti = antibiotic.name.replace("antibiotic", "").strip()
-                        cmds.append("INSERT INTO resistance_associated_mutations (specimen, software, gene, position, ref_aa, mut_aa) VALUES (\"{0}\", \"rgi\", \"{1}\", {2}, \"{3}\", \"{4}\");".format(sample, gene, pos, ref, mut))
-                        cmds.append("INSERT INTO phenotype_prediction_from_mutation (specimen, software, antibiotic) VALUES (\"{0}\", \"rgi\", \"{1}\");".format(sample, anti))
-                        cmds.append("INSERT INTO resistance_conferring_mutations_annotation (gene, position, ref_aa, mut_aa, annotation_source, antibiotic) VALUES (\"{0}\", {1}, \"{2}\", \"{3}\", \"rgi\", \"{4}\");".format(gene, pos, ref, mut, anti))
+        genes = list(set(best_hit).intersection(set(gene_list)))
+        if len(genes) == 1:
+            gene = genes[0]
+            terms = row["ARO"].split(",")
+            SNPS = row["SNP"].split(",")
+            for i in range(len(terms)):
+                term = aro_ont[terms[i].strip()]
+                snp = SNPS[i].strip()
+                pos = re.sub("[^0-9]", "", snp)
+                ref = snp[0]
+                mut = snp[-1]
+                for child in term.relations:
+                    if child.obo_name=="confers_resistance_to_drug" or  child.obo_name=="confers_resistance_to":
+                        for antibiotic in term.relations[child]:
+                            anti = antibiotic.name.replace("antibiotic", "").strip()
+                            cmds.append("INSERT IGNORE INTO resistance_associated_mutations (specimen, software, gene, position, ref_aa, mut_aa) VALUES (\"{0}\", \"rgi\", \"{1}\", {2}, \"{3}\", \"{4}\");".format(sample, gene, pos, ref, mut))
+                            cmds.append("INSERT IGNORE INTO phenotype_prediction_from_mutation (specimen, software, antibiotic) VALUES (\"{0}\", \"rgi\", \"{1}\");".format(sample, anti))
+                            cmds.append("INSERT IGNORE INTO resistance_conferring_mutations_annotation (gene, position, ref_aa, mut_aa, annotation_source, antibiotic) VALUES (\"{0}\", {1}, \"{2}\", \"{3}\", \"rgi\", \"{4}\");".format(gene, pos, ref, mut, anti))
             for cmd in cmds:
                 try:
                     cursor.execute(cmd)
                 except mysql.connector.errors.Error as err :
                     with open(log, "a") as f:
                         f.write("Something went wrong: {}\n".format(err))
-
-
         else:
             raise Exception("Problem parsing the predicted ARO Model from RGI from {0}".format(sample))  
                             
