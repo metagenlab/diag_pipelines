cdd2b9c825aba0071e4d649ac94db000ace08ee0
Cashalow <sachalau@gmail.com>
2017-12-19 15:38:57 +0100

    adding virulence search and formatting of results

---
+++ rules/annotation/scripts/merge_result_and_description.py
@@ -0,0 +1,13 @@
+import pandas
+
+res = pandas.read_csv(snakemake.input[0], delimiter="\t")
+db = pandas.read_csv(snakemake.input[1], delimiter="\t")
+
+merged = pandas.merge(res, db, left_on="Virulence factor ID", right_on="gene")
+
+
+merged[["Virulence factor ID", "Sequenced gene ID", "Amino acid percentage identity", "Coverage of the Virulence factor protein on the alignement", "E-value", "uniprot_accession", "description"]].to_csv(snakemake.output[0], header=True, sep="\t", index=False)
+
+writer = pandas.ExcelWriter(snakemake.output[1])
+merged[["Virulence factor ID", "Sequenced gene ID", "Amino acid percentage identity", "Coverage of the Virulence factor protein on the alignement", "E-value", "uniprot_accession", "description"]].to_excel(writer, snakemake.params["sample_corres"][snakemake.wildcards["sample"]], index=False)
+writer.save()
