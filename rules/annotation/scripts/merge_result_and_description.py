1fb13f0ae20b4c9fdbcf6bd45d9c325693cddee6
Cashalow <sachalau@gmail.com>
2018-01-03 18:47:51 +0100

    removing need for name correspondance parameter

--- rules/annotation/scripts/merge_result_and_description.py
+++ rules/annotation/scripts/merge_result_and_description.py
@@ -9,5 +9,5 @@ merged = pandas.merge(res, db, left_on="Virulence factor ID", right_on="gene")
 merged[["Virulence factor ID", "Sequenced gene ID", "Amino acid percentage identity", "Coverage of the Virulence factor protein on the alignement", "E-value", "uniprot_accession", "description"]].to_csv(snakemake.output[0], header=True, sep="\t", index=False)
 
 writer = pandas.ExcelWriter(snakemake.output[1])
-merged[["Virulence factor ID", "Sequenced gene ID", "Amino acid percentage identity", "Coverage of the Virulence factor protein on the alignement", "E-value", "uniprot_accession", "description"]].to_excel(writer, snakemake.params["sample_corres"][snakemake.wildcards["sample"]], index=False)
+merged[["Virulence factor ID", "Sequenced gene ID", "Amino acid percentage identity", "Coverage of the Virulence factor protein on the alignement", "E-value", "uniprot_accession", "description"]].to_excel(writer, snakemake.wildcards["sample"], index=False)
 writer.save()
