b8cfb8f74925e5015d8226fe336c7639ae01dcd6
Cashalow <sachalau@gmail.com>
2018-01-05 18:21:27 +0100

    python script for extracting snps position

--- rules/typing/scripts/snp_distance_from_vcf.py
+++ rules/typing/scripts/snp_distance_from_vcf.py
@@ -1,7 +1,16 @@
 import pandas
 import itertools
+import pysam
+import re
+from collections import defaultdict
 
-snps = pandas.read_csv(snakemake.input[0], sep="\t", header=0)
+from Bio import SeqIO
+
+
+acc=re.sub("\..*", "", list(SeqIO.parse(snakemake.input["gbk"], "genbank"))[0].id)
+
+
+snps = pandas.read_csv(snakemake.input["genotype"], sep="\t", header=0)
 
 ref = snakemake.wildcards["ref"]
 all_samples = sorted(snakemake.params["samples"] + [ref])
@@ -36,9 +45,35 @@ for j in all_samples:
 with open(snakemake.output[0], "w") as resfile:
     resfile.write(res_str)
 
+bam_files=str(snakemake.input["bams"]).split(" ")
+
+
+def return_acgt_count(alignment_file, position):
+    samfile = pysam.AlignmentFile(alignment_file, "rb")
+    iterator=samfile.pileup(contig=acc, start=position-1, stop=position)
+    counts={}
+    for x in ["A", "C", "G", "T"]:
+        counts[x]=0
+    for x in iterator:
+        if x.reference_pos == (position-1):
+            for y in x.pileups:
+                if y.query_position is not None:
+                    counts[y.alignment.query_sequence[y.query_position]] += 1
+    return(counts)
+        
+    
+
 
 with open(snakemake.output[1], "w") as posfile:
     for i, j in itertools.combinations_with_replacement(snakemake.params["samples"], 2):
         if i != j:
-            for k in position_of_snps[frozenset((i,j))]:
-                posfile.write(str(i)+"-"+str(j)+"\t"+str(k)+"\n")
+            if number_of_snps[frozenset((i,j))] < snakemake.params["dist_thre"]:
+                for k in position_of_snps[frozenset((i,j))]:
+                    file1 = [s for s in bam_files if i in s][0]
+                    file2 = [s for s in bam_files if j in s][0]
+                    c1 = return_acgt_count(file1, k)
+                    c2 = return_acgt_count(file2, k)
+                    compa = ""
+                    for nucl in sorted(c1.keys()):
+                        compa += str(c1[nucl])+"-"+str(c2[nucl])+"\t"
+                    posfile.write(str(i)+"\t"+str(j)+"\t"+str(k)+"\t"+compa+"\n")
