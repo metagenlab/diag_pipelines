44052b3b16912616d0730d936f953063754da8b0
Cashalow <sachalau@gmail.com>
2018-01-05 18:06:07 +0100

    cleaning helper scripts

--- helper_scripts/get_sample_naming_from_link_folder.py
+++ helper_scripts/get_sample_naming_from_link_folder.py
@@ -2,7 +2,7 @@ import os
 import re
 
 
-def get_read_naming_patterns(directory):
+def get_read_naming_patterns_general(directory):
     result = []
     extension= {}
     for fname in os.listdir(directory):
@@ -27,4 +27,23 @@ def get_read_naming_patterns(directory):
                         extension[samp].append(re.sub("^_", "", ext.group(0)))
                 else:
                     extension[samp].append(re.sub("^_", "", ext.group(0)))
+    return(extension)
+
+
+def get_read_naming_patterns_molis(directory):
+    result = []
+    extension= {}
+    for fname in os.listdir(directory):
+        if fname.endswith("fastq.gz") or fname.endswith("fq.gz") or fname.endswith("fastq") or fname.endswith("fq"):
+            ext = re.search(r'_.*_R(1|2)(\.|_).*', fname)
+            read = re.compile(re.search(r'_R(1|2)(\.|_)', fname).group(0).replace("_", "").replace(".", ""))
+            samp = re.sub(r'_R(1|2)$', '', re.search(r'.*_R(1|2)', fname).group(0)).split("_")[0]
+            extension.setdefault(samp, [])
+            match = list(filter(read.match, extension[samp]))
+            if len(match) == 1:
+                if match[0].endswith("fastq") or match[0].endswith("fq"):
+                    extension[samp].remove(match[0])
+                    extension[samp].append(re.sub("^_", "", ext.group(0)))
+            else:
+                extension[samp].append(re.sub("^_", "", ext.group(0)))
     return(extension)
--- helper_scripts/get_species_and_sample_correspondance.py
+++ helper_scripts/get_species_and_sample_correspondance.py
@@ -11,3 +11,15 @@ def get_species_from_file(filename):
                 corres[cells[0]]["genus"]=cells[1].replace(" ", "")
                 corres[cells[0]]["species"]=cells[2].replace(" ", "")
     return(corres)
+
+
+def get_sample_names_correspondance_from_file(filename):
+    corres = {}
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t', quotechar='|')
+        for row in [[s.strip() for s in inner] for inner in reader]:
+            if row:
+                cells = list(filter(None, row))
+                corres[cells[0]] = cells[1].replace(" ", "_")
+    return(corres)
+
