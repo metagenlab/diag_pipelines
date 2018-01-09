98dd2a4ae034226b4a015cfa590bbb4ffdf9f090
Cashalow <sachalau@gmail.com>
2018-01-09 20:17:30 +0100

    new excel generation

--- rules/typing/scripts/snp_distance_from_vcf.py
+++ rules/typing/scripts/snp_distance_from_vcf.py
@@ -3,6 +3,7 @@ import itertools
 import pysam
 import re
 
+from collections import defaultdict
 from Bio import SeqIO
 
 def atoi(text):
@@ -26,6 +27,7 @@ snps = pandas.read_csv(snakemake.input["genotype"], sep="\t", header=0)
 ref = snakemake.wildcards["ref"]
 all_samples = snakemake.params["samples"]
 all_samples.sort(key=natural_keys)
+
 number_of_snps = {}
 position_of_snps = {}
 
@@ -35,13 +37,13 @@ for i, j in itertools.combinations(all_samples, 2):
     vect = list(snps[[i]].values)
     if frozenset((i, ref)) not in number_of_snps.keys():
         number_of_snps[frozenset((i, ref))] = sum(vect)[0]
-        result.loc[[i], [ref]] = number_of_snps[frozenset((i, ref))]
-        result.loc[[ref], [i]] = number_of_snps[frozenset((i, ref))]
+        result.loc[i, ref] = number_of_snps[frozenset((i, ref))]
+        result.loc[ref, i] = number_of_snps[frozenset((i, ref))]
     vect2 = list(snps[[j]].values)
     if frozenset((j, ref)) not in number_of_snps.keys():
         number_of_snps[frozenset((j, ref))] = sum(vect2)[0]
-        result.loc[[j], [ref]] = number_of_snps[frozenset((j, ref))]
-        result.loc[[ref], [j]] = number_of_snps[frozenset((j, ref))]
+        result.loc[j, ref] = number_of_snps[frozenset((j, ref))]
+        result.loc[ref, j] = number_of_snps[frozenset((j, ref))]
     number_of_snps[frozenset((i,j))] = len([u for u in range(len(vect)) if vect[u] != vect2[u]])
     position_of_snps[frozenset((i,j))] = list(snps.loc[[ u for u in range(len(vect)) if vect[u] != vect2[u] ], "POS"].values)
     result.loc[[i], [j]] = number_of_snps[frozenset((i,j))]
@@ -59,26 +61,56 @@ bam_files=str(snakemake.input["bams"]).split(" ")
 def return_acgt_count(alignment_file, position):
     samfile = pysam.AlignmentFile(alignment_file, "rb")
     iterator=samfile.pileup(contig=acc, start=position-1, stop=position)
-    counts={}
-    for x in ["A", "C", "G", "T"]:
-        counts[x]=0
+    counts = defaultdict(int)
     for x in iterator:
         if x.reference_pos == (position-1):
             for y in x.pileups:
                 if y.query_position is not None:
                     counts[y.alignment.query_sequence[y.query_position]] += 1
     return(counts)
-        
+
+
+
+
+res = []
 with open(snakemake.output[1], "w") as posfile:
-    for i, j in itertools.combinations_with_replacement(snakemake.params["samples"], 2):
-        if i != j:
-            if number_of_snps[frozenset((i,j))] < snakemake.params["dist_thre"]:
-                for k in position_of_snps[frozenset((i,j))]:
-                    file1 = [s for s in bam_files if i in s][0]
-                    file2 = [s for s in bam_files if j in s][0]
-                    c1 = return_acgt_count(file1, k)
-                    c2 = return_acgt_count(file2, k)
-                    compa = ""
-                    for nucl in sorted(c1.keys()):
-                        compa += str(c1[nucl])+"-"+str(c2[nucl])+"\t"
-                    posfile.write(str(i)+"\t"+str(j)+"\t"+str(k)+"\t"+compa+"\n")
+    for i, j in itertools.combinations(all_samples, 2):
+        if number_of_snps[frozenset((i,j))] < snakemake.params["dist_thre"]:
+            for k in position_of_snps[frozenset((i,j))]:
+                file1 = [s for s in bam_files if i in s][0]
+                file2 = [s for s in bam_files if j in s][0]
+                c1 = return_acgt_count(file1, k)
+                c2 = return_acgt_count(file2, k)
+                res1 = [c1[x]/sum(c1.values()) for x in ["A", "C", "G", "T"]]
+                res2 = [c2[x]/sum(c2.values()) for x in ["A", "C", "G", "T"]]
+                res.append([i, k, res1[0], res1[1], res1[2], res1[3], sum(c1.values())])
+                res.append([j, k, res2[0], res2[1], res2[2], res2[3], sum(c2.values())])
+
+
+
+def color_negative_red(val):
+    """
+    Takes a scalar and returns a string with
+    the css property `'color: red'` for negative
+    strings, black otherwise.
+    """
+    if isinstance(val, str) or val < 0.5 :
+        color = 'black'
+    else:
+        color = 'red'
+    return 'color: %s' % color
+
+
+print(pandas.__file__)
+
+df = pandas.DataFrame(res, columns=['Strain', 'Position in reference genome', 'A', 'C', 'G', 'T', 'Total Coverage'])
+
+out_xlsx_positions = snakemake.output[3]
+out_csv_positions = snakemake.output[1]
+
+writer = pandas.ExcelWriter(out_xlsx_positions)
+
+df.style.applymap(color_negative_red).to_excel(writer, ref, index=False, engine="openpyxl")
+writer.save()
+
+df.to_csv(out_csv_positions, index=False, sep="\t")
