5d09bcd4d27e68b6b95cdbba5396729dfce75abf
Cashalow <sachalau@gmail.com>
2018-01-15 19:10:17 +0100

    modifying naming sample function to handle directly files downloaded from SRA, enabling mixed naming schemas for different samples (molis and SRA together for instance)

--- helper_scripts/get_sample_naming_from_link_folder.py
+++ helper_scripts/get_sample_naming_from_link_folder.py
@@ -7,7 +7,9 @@ def get_read_naming_patterns_general(directory):
     extension= {}
     for fname in os.listdir(directory):
         if fname.endswith("fastq.gz") or fname.endswith("fq.gz") or fname.endswith("fastq") or fname.endswith("fq"):
-            ext = re.search(r'_R(1|2)(\.|_).*', fname)
+            print(fname)
+            ext = re.search(r'_(R)?(1|2)(\.|_).*', fname)
+            print(ext)
             if ext is None:
                 ext = re.search(r'f(?:ast)?q(?:\.gz)?', fname)
                 samp = re.sub("\.$", "", re.search(r'^([^\.]*)\.*', fname).group(0))
@@ -17,8 +19,8 @@ def get_read_naming_patterns_general(directory):
                 else:
                     extension[samp]=[ext.group(0)]
             else:
-                read = re.compile(re.search(r'_R(1|2)(\.|_)', fname).group(0).replace("_", "").replace(".", ""))
-                samp = re.sub(r'_R(1|2)$', '', re.search(r'.*_R(1|2)', fname).group(0))
+                read = re.compile(re.search(r'_(R)?(1|2)(\.|_)', fname).group(0).replace("_", "").replace(".", ""))
+                samp = re.sub(r'_(R)?(1|2)$', '', re.search(r'.*_(R)?(1|2)', fname).group(0))
                 extension.setdefault(samp, [])
                 match = list(filter(read.match, extension[samp]))
                 if len(match) == 1:
@@ -35,15 +37,25 @@ def get_read_naming_patterns_molis(directory):
     extension= {}
     for fname in os.listdir(directory):
         if fname.endswith("fastq.gz") or fname.endswith("fq.gz") or fname.endswith("fastq") or fname.endswith("fq"):
-            ext = re.search(r'_.*_R(1|2)(\.|_).*', fname)
-            read = re.compile(re.search(r'_R(1|2)(\.|_)', fname).group(0).replace("_", "").replace(".", ""))
-            samp = re.sub(r'_R(1|2)$', '', re.search(r'.*_R(1|2)', fname).group(0)).split("_")[0]
-            extension.setdefault(samp, [])
-            match = list(filter(read.match, extension[samp]))
-            if len(match) == 1:
-                if match[0].endswith("fastq") or match[0].endswith("fq"):
-                    extension[samp].remove(match[0])
-                    extension[samp].append(re.sub("^_", "", ext.group(0)))
+            ext = re.search(r'_(R)?(1|2)(\.|_).*', fname)
+            if ext is None:
+                ext = re.search(r'f(?:ast)?q(?:\.gz)?', fname)
+                samp = re.sub("\.$", "", re.search(r'^([^\.]*)\.*', fname).group(0))
+                if samp in extension.keys():
+                    if ext.group(0).endswith(".gz"):
+                        extension[samp]=[ext.group(0)]
+                else:
+                    extension[samp]=[ext.group(0)]
             else:
-                extension[samp].append(re.sub("^_", "", ext.group(0)))
+                read = re.compile(re.search(r'_R?(1|2)(\.|_)', fname).group(0).replace("_", "").replace(".", ""))
+                samp = re.sub(r'_R?(1|2)$', '', re.search(r'.*_R?(1|2)', fname).group(0)).split("_")[0]
+                extension.setdefault(samp, [])
+                match = list(filter(read.match, extension[samp]))
+                if len(match) == 1:
+                    if match[0].endswith("fastq") or match[0].endswith("fq"):
+                        extension[samp].remove(match[0])
+                        extension[samp].append(re.sub("^_", "", ext.group(0)))
+                else:
+                    extension[samp].append(re.sub("^_", "", ext.group(0)))
+            
     return(extension)
