e384081d1ab061aae06962fae6dd5073b22e38ca
Cashalow <sachalau@gmail.com>
2018-01-17 19:03:12 +0100

    simplification of the sample naming handling

--- helper_scripts/get_sample_naming_from_link_folder.py
+++ helper_scripts/get_sample_naming_from_link_folder.py
@@ -2,14 +2,14 @@ import os
 import re
 
 
-def get_read_naming_patterns_general(directory):
+def get_read_naming_patterns(directory):
     result = []
     extension= {}
     for fname in os.listdir(directory):
         if fname.endswith("fastq.gz") or fname.endswith("fq.gz") or fname.endswith("fastq") or fname.endswith("fq"):
-            print(fname)
-            ext = re.search(r'_(R)?(1|2)(\.|_).*', fname)
-            print(ext)
+            regex_str = '(_S[0-9]+)?(_L0+[1-9]+)?_(R)?(1|2)(\.|_)'
+            regex = re.compile(regex_str)
+            ext = re.search(regex, fname)
             if ext is None:
                 ext = re.search(r'f(?:ast)?q(?:\.gz)?', fname)
                 samp = re.sub("\.$", "", re.search(r'^([^\.]*)\.*', fname).group(0))
@@ -19,43 +19,11 @@ def get_read_naming_patterns_general(directory):
                 else:
                     extension[samp]=[ext.group(0)]
             else:
-                read = re.compile(re.search(r'_(R)?(1|2)(\.|_)', fname).group(0).replace("_", "").replace(".", ""))
-                samp = re.sub(r'_(R)?(1|2)$', '', re.search(r'.*_(R)?(1|2)', fname).group(0))
+                regex_after = re.compile(regex_str+".*")
+                regex_before = re.compile(".*"+regex_str)
+                read = re.compile(re.search(regex_after, fname).group(0))
+                samp = re.sub(regex, '', re.search(regex_before, fname).group(0))
                 extension.setdefault(samp, [])
-                match = list(filter(read.match, extension[samp]))
-                if len(match) == 1:
-                    if match[0].endswith("fastq") or match[0].endswith("fq"):
-                        extension[samp].remove(match[0])
-                        extension[samp].append(re.sub("^_", "", ext.group(0)))
-                else:
-                    extension[samp].append(re.sub("^_", "", ext.group(0)))
+                extension[samp].append(re.sub("^_", "", read.pattern))
     return(extension)
 
-
-def get_read_naming_patterns_molis(directory):
-    result = []
-    extension= {}
-    for fname in os.listdir(directory):
-        if fname.endswith("fastq.gz") or fname.endswith("fq.gz") or fname.endswith("fastq") or fname.endswith("fq"):
-            ext = re.search(r'_(R)?(1|2)(\.|_).*', fname)
-            if ext is None:
-                ext = re.search(r'f(?:ast)?q(?:\.gz)?', fname)
-                samp = re.sub("\.$", "", re.search(r'^([^\.]*)\.*', fname).group(0))
-                if samp in extension.keys():
-                    if ext.group(0).endswith(".gz"):
-                        extension[samp]=[ext.group(0)]
-                else:
-                    extension[samp]=[ext.group(0)]
-            else:
-                read = re.compile(re.search(r'_R?(1|2)(\.|_)', fname).group(0).replace("_", "").replace(".", ""))
-                samp = re.sub(r'_R?(1|2)$', '', re.search(r'.*_R?(1|2)', fname).group(0)).split("_")[0]
-                extension.setdefault(samp, [])
-                match = list(filter(read.match, extension[samp]))
-                if len(match) == 1:
-                    if match[0].endswith("fastq") or match[0].endswith("fq"):
-                        extension[samp].remove(match[0])
-                        extension[samp].append(re.sub("^_", "", ext.group(0)))
-                else:
-                    extension[samp].append(re.sub("^_", "", ext.group(0)))
-            
-    return(extension)
