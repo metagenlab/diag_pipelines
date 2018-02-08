25839afcdd5659db86f481e70c51bb190e61f47e
Cashalow <sachalau@gmail.com>
2018-02-08 18:41:32 +0100

    adding description back from database

--- rules/annotation/scripts/merge_result_and_description.py
+++ rules/annotation/scripts/merge_result_and_description.py
@@ -3,11 +3,19 @@ import pandas
 res = pandas.read_csv(snakemake.input[0], delimiter="\t")
 db = pandas.read_csv(snakemake.input[1], delimiter="\t")
 
-merged = pandas.merge(res, db, left_on="Virulence factor ID", right_on="gene")
+db2 = pandas.DataFrame({"gene_uniprot":db[["gene", "uniprot_accession"]].apply(lambda x: '_'.join(x), axis=1), "description":db["description"], "gene":db["gene"]})
+print(db2)
+
+merged = pandas.merge(res, db2, left_on="virulence_factor_ID", right_on="gene_uniprot")
 
 
-merged[["Virulence factor ID", "Sequenced gene ID", "Amino acid percentage identity", "Coverage of the Virulence factor protein on the alignement", "E-value", "uniprot_accession", "description"]].to_csv(snakemake.output[0], header=True, sep="\t", index=False)
+cols = list(merged)
+
+cols.insert(1, cols.pop(cols.index("description")))
+
+merged = merged.ix[:, cols]
+merged.to_csv(snakemake.output[0], header=True, sep="\t", index=False)
 
 writer = pandas.ExcelWriter(snakemake.output[1])
-merged[["Virulence factor ID", "Sequenced gene ID", "Amino acid percentage identity", "Coverage of the Virulence factor protein on the alignement", "E-value", "uniprot_accession", "description"]].to_excel(writer, snakemake.wildcards["sample"], index=False)
+merged.to_excel(writer, snakemake.wildcards["sample"], index=False)
 writer.save()
