647195c7e7a702ff209cd292aa6b4c30d1b5629f
Cashalow <sachalau@gmail.com>
2018-02-08 18:16:56 +0100

    cleaning

--- rules/annotation/scripts/merge_summary_xlsx.py
+++ rules/annotation/scripts/merge_summary_xlsx.py
@@ -6,11 +6,6 @@ def atoi(text):
     return int(text) if text.isdigit() else text
 
 def natural_keys(text):
-    '''
-    alist.sort(key=natural_keys) sorts in human order
-    http://nedbatchelder.com/blog/200712/human_sorting.html
-    (See Toothy's implementation in the comments)
-    '''
     return [ atoi(c) for c in re.split('(\d+)', text) ]
 
 
@@ -27,3 +22,4 @@ for j in excels:
         df = j.parse(sheet)
         df.to_excel(writer, sheet, index=False)
         writer.save()
+
