5bf42d314d4151aa59345d37ee617a48d8a09caa
Cashalow <sachalau@gmail.com>
2018-03-06 16:36:26 +0100

    rebuilt docs

--- rules/annotation/scripts/hashing_genes.py
+++
@@ -1,6 +0,0 @@
-import hashlib
-from Bio import SeqIO
-
-with open(snakemake.output[0], "w") as hash_file:
-    for record in SeqIO.parse(snakemake.input[0], "fasta"):
-        hash_file.write(hashlib.sha256(str(record.seq).encode()).hexdigest()+"\n")
