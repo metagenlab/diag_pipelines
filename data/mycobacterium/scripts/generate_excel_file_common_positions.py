ca62252586e98f2fa6508e43ce3ea8ec8976ce77
Cashalow <sachalau@gmail.com>
2018-03-12 15:13:14 +0100

    better generation of the excel summary file

--- data/mycobacterium/scripts/generate_excel_file_common_positions.py
+++ data/mycobacterium/scripts/generate_excel_file_common_positions.py
@@ -6,66 +6,55 @@ from matplotlib_venn import venn2
 
 import matplotlib.pyplot as plt
 
-rgi = pandas.read_csv("../db/rgi_annotated_full_2_0_0.csv", sep="\t")
-rgi["MutatedAminoAcidOrNucleotide"]=rgi["MutatedAminoAcidOrNucleotide"].str.upper().str.strip()
-rgi["WildTypeAminoAcidOrNucleotide"]=rgi["WildTypeAminoAcidOrNucleotide"].str.upper().str.strip()
-miotto = pandas.read_csv("../db/miotto_high_moderate_minimum_confidence_annotated.csv", sep="\t")
-miotto["MutatedAminoAcidOrNucleotide"]=miotto["MutatedAminoAcidOrNucleotide"].str.upper().str.strip()
-miotto["WildTypeAminoAcidOrNucleotide"]=miotto["WildTypeAminoAcidOrNucleotide"].str.upper().str.strip()
-mykrobe = pandas.read_csv("../db/mykrobe_annotated.tsv", sep="\t")
-mykrobe["MutatedAminoAcidOrNucleotide"]=mykrobe["MutatedAminoAcidOrNucleotide"].str.upper().str.strip()
-mykrobe["WildTypeAminoAcidOrNucleotide"]=mykrobe["WildTypeAminoAcidOrNucleotide"].str.upper().str.strip()
-walker = pandas.read_csv("../db/walker_resistant_annotated.tsv", sep="\t")
-walker["MutatedAminoAcidOrNucleotide"]=walker["MutatedAminoAcidOrNucleotide"].str.upper().str.strip()
-walker["WildTypeAminoAcidOrNucleotide"]=walker["WildTypeAminoAcidOrNucleotide"].str.upper().str.strip()
+source_db = {}
+source_db["SNP"] = {"CARD":"../db/rgi_annotated_full_2_0_0.csv" , "Miotto et al. 2017":"../db/miotto_high_moderate_minimum_confidence_annotated.csv", "Mykrobe":"../db/mykrobe_annotated.tsv", "Walker et al. 2015":"../db/walker_resistant_annotated.tsv"}
+source_db["INDEL"] = {"Mykrobe":"../db/mykrobe_annotated.tsv", "Walker et al. 2015":"../db/walker_resistant_annotated.tsv"}
 
+nb_sources = len(source_db)
 
-results = [rgi, miotto, mykrobe, walker]
-nb_software = len(results)
+genes = set(sorted(["rrs","eis", "rpoB", "rpsL","gyrA", "gyrB", "katG", "pncA", "rrs", "tlyA", "gidB", "inhA", "embA", "embB", "embC", "embR", "ethA", "folC", "iniA", "iniB", "iniC", "kasA", "ndh", "ribD", "thyA", "ahpC", "mabA"]))
 
-genes = ["rrs","eis", "rpoB", "rpsL","gyrA", "gyrB", "katG", "pncA", "rrs", "tlyA", "gidB", "inhA", "embA", "embB", "embC", "embR", "ethA", "folC", "iniA", "iniB", "iniC", "kasA", "ndh", "ribD", "thyA", "ahpC", "mabA"]
+common_positions = {}
+common_positions["SNP"] = []
+common_positions["INDEL"] = []
 
-common_positions = []
-for gene in genes:
-    for mutation_type in ["SNP","INDEL"]:
-        rgi_res = set(sorted([int(x) for x in rgi.loc[(rgi["MutationType"]==mutation_type) & (rgi["Gene"]==gene), "PositionMTB"]]))
-        mykrobe_res = set(sorted([int(x) for x in mykrobe.loc[(mykrobe["MutationType"]==mutation_type) & (mykrobe["Gene"]==gene), "PositionMTB"]]))
-        walker_res = set(sorted([int(x) for x in walker.loc[(walker["MutationType"]==mutation_type) & (walker["Gene"]==gene), "PositionMTB"]]))
-        miotto_res = set(sorted([int(x) for x in miotto.loc[(miotto["MutationType"]==mutation_type) & (miotto["Gene"]==gene), "PositionMTB"]]))
-        print(rgi_res)
-                         
-                            
-        common =  sorted(rgi_res & mykrobe_res & miotto_res & walker_res )
+all_res = {}
+for i in source_db["SNP"].keys():
+    all_res[i] = pandas.read_csv(source_db["SNP"][i], sep="\t")
+    all_res[i]["MutatedAminoAcidOrNucleotide"]=all_res[i]["MutatedAminoAcidOrNucleotide"].str.upper().str.strip()
+    all_res[i]["WildTypeAminoAcidOrNucleotide"]=all_res[i]["WildTypeAminoAcidOrNucleotide"].str.upper().str.strip()
+
+for mutation_type in ["SNP","INDEL"]:
+    for gene in genes:
+        key = sorted(source_db[mutation_type].keys())[0]
+        common = set(sorted([int(x) for x in all_res[key].loc[(all_res[key]["MutationType"]==mutation_type) & (all_res[key]["Gene"]==gene), "PositionMTB"]]))
+        for i in sorted(source_db[mutation_type].keys())[1:]:
+            common = common & set(sorted([int(x) for x in all_res[i].loc[(all_res[i]["MutationType"]==mutation_type) & (all_res[i]["Gene"]==gene), "PositionMTB"]]))
         for pos in common:
-            mutated_rgi=sorted(set(rgi.loc[(rgi["MutationType"]==mutation_type) & (rgi["Gene"]==gene) & (rgi["PositionMTB"]==pos), "MutatedAminoAcidOrNucleotide"]))
-            wildtype_rgi=sorted(set(rgi.loc[(rgi["MutationType"]==mutation_type) & (rgi["Gene"]==gene) & (rgi["PositionMTB"]==pos), "WildTypeAminoAcidOrNucleotide"]))
-            common_positions.append(["CARD", gene, str(pos), ",".join(wildtype_rgi), ",".join(mutated_rgi)])
+            for i in source_db[mutation_type].keys():
+                if mutation_type=="SNP":
+                    mutated=sorted(set(all_res[i].loc[(all_res[i]["MutationType"]==mutation_type) & (all_res[i]["Gene"]==gene) & (all_res[i]["PositionMTB"]==pos), "MutatedAminoAcidOrNucleotide"]))
+                    wildtype=sorted(set(all_res[i].loc[(all_res[i]["MutationType"]==mutation_type) & (all_res[i]["Gene"]==gene) & (all_res[i]["PositionMTB"]==pos), "WildTypeAminoAcidOrNucleotide"]))
+                    common_positions[mutation_type].append([i, gene, str(pos), ",".join(wildtype), ",".join(mutated)])
+                elif mutation_type=="INDEL":
+                    annot=sorted(set(all_res[i].loc[(all_res[i]["MutationType"]==mutation_type) & (all_res[i]["Gene"]==gene) & (all_res[i]["PositionMTB"]==pos), "OriginalAnnotation"]))
+                    common_positions[mutation_type].append([i, gene, str(pos), annot])
+                    
 
-            mutated_mykrobe=sorted(set(mykrobe.loc[(mykrobe["MutationType"]==mutation_type) & (mykrobe["Gene"]==gene) & (mykrobe["PositionMTB"]==pos), "MutatedAminoAcidOrNucleotide"]))
-            wildtype_mykrobe=sorted(set(mykrobe.loc[(mykrobe["MutationType"]==mutation_type) & (mykrobe["Gene"]==gene) & (mykrobe["PositionMTB"]==pos), "WildTypeAminoAcidOrNucleotide"]))
-            common_positions.append(["Mykrobe", gene, str(pos), ",".join(wildtype_mykrobe), ",".join(mutated_mykrobe)])
-            
-            mutated_miotto=sorted(set(miotto.loc[(miotto["MutationType"]==mutation_type) & (miotto["Gene"]==gene) & (miotto["PositionMTB"]==pos), "MutatedAminoAcidOrNucleotide"]))
-            wildtype_miotto=sorted(set(miotto.loc[(miotto["MutationType"]==mutation_type) & (miotto["Gene"]==gene) & (miotto["PositionMTB"]==pos), "WildTypeAminoAcidOrNucleotide"]))
-            common_positions.append(["Miotto", gene, str(pos), ",".join(wildtype_miotto),",".join(mutated_miotto)])
-            
-            mutated_walker=sorted(set(walker.loc[(walker["MutationType"]==mutation_type) & (walker["Gene"]==gene) & (walker["PositionMTB"]==pos), "MutatedAminoAcidOrNucleotide"]))
-            wildtype_walker=sorted(set(walker.loc[(walker["MutationType"]==mutation_type) & (walker["Gene"]==gene) & (walker["PositionMTB"]==pos), "WildTypeAminoAcidOrNucleotide"]))
-            common_positions.append(["Walker", gene, str(pos), ",".join(wildtype_walker),",".join(mutated_walker)])
-
-            
+                
 def alternate_gray_background(row):
     ret=row.copy()
     ret[[bool(x) for x in row.index//4%2]]='background-color:#DCDCDC'
     ret[[not bool(x) for x in row.index//4%2]]=''
     return(ret)
-
+  
     
-    
-res_panda=pandas.DataFrame(common_positions, columns=["Source", "Gene", "Position", "WildTypeAminoAcidorNucleotide", "MutatedAminoAcidOrNucleotide"])
+res_panda_snp=pandas.DataFrame(common_positions["SNP"], columns=["Source", "Gene", "Position", "WildTypeAminoAcidorNucleotide", "MutatedAminoAcidOrNucleotide"])
+res_panda_indel=pandas.DataFrame(common_positions["INDEL"], columns=["Source", "Gene", "Position", "OriginalAnnotation"])
 
 writer = pandas.ExcelWriter("summary.xlsx")
-res_panda.style.apply(alternate_gray_background, axis=None).to_excel(writer, index=False)
+res_panda_snp.style.apply(alternate_gray_background, axis=None).to_excel(writer, "SNP", index=False)
+res_panda_indel.style.apply(alternate_gray_background, axis=None).to_excel(writer, "INDEL", index=False)
 writer.save()
 
 
