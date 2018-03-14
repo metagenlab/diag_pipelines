d0134f5c4a5fb841b0384c06ed54144f5add8913
Cashalow <sachalau@gmail.com>
2018-03-14 18:55:26 +0100

    filtering freq each sample separately, no more decompose blocksub

--- rules/typing/scripts/calculate_distances_from_alignment.py
+++ rules/typing/scripts/calculate_distances_from_alignment.py
@@ -10,4 +10,4 @@ with open(snakemake.output[0], "w") as dist_file:
         for seq1,seq2 in zip(align[i].seq, align[j].seq):
             if seq1 != seq2 and seq1 !="N" and seq2 !="N":
                 mutations += 1
-        dist_file.write(align[i].id+" "+align[j].id+" "+ str(mutations)+"\n")
+        dist_file.write(align[i].id+" "+align[j].id+" "+ str(int(mutations))+"\n")
