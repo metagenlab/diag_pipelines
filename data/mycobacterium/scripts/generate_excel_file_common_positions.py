cf01545cc9b91860a7a001d0833f1ec756db0909
Cashalow <sachalau@gmail.com>
2018-03-16 09:43:28 +0100

    cleaning up

--- data/mycobacterium/scripts/generate_excel_file_common_positions.py
+++ data/mycobacterium/scripts/generate_excel_file_common_positions.py
@@ -24,27 +24,22 @@ print(set(list(gene_antibio["Antibiotic"])))
 
 print(genes)
 
-common_positions={}
-for i in set(list(gene_antibio["Antibiotic"])):
-    common_positions[i] = {}
-    common_positions[i]["SNP"] = []
-    common_positions[i]["INDEL"] = []
-
 all_res = {}
 for i in source_db["SNP"].keys():
     all_res[i] = pandas.read_csv(source_db["SNP"][i], sep="\t")
     all_res[i]["MutatedAminoAcidOrNucleotide"]=all_res[i]["MutatedAminoAcidOrNucleotide"].str.upper().str.strip()
     all_res[i]["WildTypeAminoAcidOrNucleotide"]=all_res[i]["WildTypeAminoAcidOrNucleotide"].str.upper().str.strip()
+    all_res[i]["PositionMTB"]=pandas.to_numeric(all_res[i]["PositionMTB"])
 
 
-
-
-common_positions_3={}
-
-for i in set(list(gene_antibio["Antibiotic"])):
-    common_positions_3[i] = {}
-    common_positions_3[i]["SNP"] = []
-    common_positions_3[i]["INDEL"] = []
+common_positions={}
+for j in ["4", "3", "2"]:
+    common_positions[j] = {}
+    for i in set(list(gene_antibio["Antibiotic"])):
+        common_positions[j][i] = {}
+        common_positions[j][i]["SNP"] = []
+        common_positions[j][i]["INDEL"] = []
+    
     
 for mutation_type in ["SNP"]:
     all_keys=sorted(list(source_db[mutation_type].keys()))
@@ -53,87 +48,44 @@ for mutation_type in ["SNP"]:
         pos_each_db={}
         for key in all_keys:
             pos_each_db[key] = set(sorted([int(x) for x in all_res[key].loc[(all_res[key]["MutationType"]==mutation_type) & (all_res[key]["Gene"]==gene), "PositionMTB"]]))
-        common_4 = pos_each_db[all_keys[0]] & pos_each_db[all_keys[1]] & pos_each_db[all_keys[2]] & pos_each_db[all_keys[3]] 
-        for anti in set(gene_antibio.loc[gene_antibio["Gene"]==gene, "Antibiotic"]):
-            for pos in sorted(common_4):
-                for i in source_db[mutation_type].keys():
-                    if mutation_type=="SNP":
-                        mutated=sorted(set(all_res[i].loc[(all_res[i]["MutationType"]==mutation_type) & (all_res[i]["Gene"]==gene) & (all_res[i]["PositionMTB"]==pos), "MutatedAminoAcidOrNucleotide"]))
-                        wildtype=sorted(set(all_res[i].loc[(all_res[i]["MutationType"]==mutation_type) & (all_res[i]["Gene"]==gene) & (all_res[i]["PositionMTB"]==pos), "WildTypeAminoAcidOrNucleotide"]))
-                        common_positions[anti][mutation_type].append([i, gene, str(pos), ",".join(wildtype), ",".join(mutated)])
-                    elif mutation_type=="INDEL":
-                        annot=sorted(set(all_res[i].loc[(all_res[i]["MutationType"]==mutation_type) & (all_res[i]["Gene"]==gene) & (all_res[i]["PositionMTB"]==pos), "OriginalAnnotation"]))
-                        common_positions[anti][mutation_type].append([i, gene, str(pos), annot])
+        union_4 = sorted(set(pos_each_db[all_keys[0]] & pos_each_db[all_keys[1]] & pos_each_db[all_keys[2]] & pos_each_db[all_keys[3]]))
         common_3 = []
         for db in itertools.combinations(source_db[mutation_type].keys(), 3):
-            common_3.append(pos_each_db[db[0]] & pos_each_db[db[1]] & pos_each_db[db[2]])
-        union_3 = common_3[0].union(common_3[1].union(common_3[2])).difference(common_4)
-        for pos in sorted(union_3):
-            for i in source_db[mutation_type].keys():
-#                print(list(all_res[i].loc[all_res[i]["Gene"]==gene, "PositionMTB"]))
-                if pos in list(all_res[i].loc[(all_res[i]["Gene"]==gene) & (all_res[i]["MutationType"]==mutation_type), "PositionMTB"]):
-                    if mutation_type=="SNP":
-                        mutated=sorted(set(all_res[i].loc[(all_res[i]["MutationType"]==mutation_type) & (all_res[i]["Gene"]==gene) & (all_res[i]["PositionMTB"]==pos), "MutatedAminoAcidOrNucleotide"]))
-                        wildtype=sorted(set(all_res[i].loc[(all_res[i]["MutationType"]==mutation_type) & (all_res[i]["Gene"]==gene) & (all_res[i]["PositionMTB"]==pos), "WildTypeAminoAcidOrNucleotide"]))
-                        common_positions_3[anti][mutation_type].append([i, gene, str(pos), ",".join(wildtype), ",".join(mutated)])
-                    elif mutation_type=="INDEL":
-                        annot=sorted(set(all_res[i].loc[(all_res[i]["MutationType"]==mutation_type) & (all_res[i]["Gene"]==gene) & (all_res[i]["PositionMTB"]==pos), "OriginalAnnotation"]))
-                        common_positions_3[anti][mutation_type].append([i, gene, str(pos), annot])
-                    
+            common_3.append(pos_each_db[db[0]].intersection(pos_each_db[db[1]]).intersection(pos_each_db[db[2]]).difference(union_4))
+        union_3 = sorted(set([item for sublist in common_3 for item in sublist]))
+        common_2 = []
+        for db in itertools.combinations(source_db[mutation_type].keys(), 2):
+            common_2.append(pos_each_db[db[0]].intersection(pos_each_db[db[1]]).difference(union_4).difference(union_3))
+        union_2 = sorted(set([item for sublist in common_2 for item in sublist]))
+        unions = {}
+        unions['4'] = union_4
+        unions['3'] = union_3
+        unions['2'] = union_2
+        for anti in set(gene_antibio.loc[gene_antibio["Gene"]==gene, "Antibiotic"]):
+            for number in ["4", "3", "2"]:
+                for pos in unions[number]:
+                    for i in source_db[mutation_type].keys():
+                        if pos in list(all_res[i].loc[(all_res[i]["Gene"]==gene) & (all_res[i]["MutationType"]==mutation_type), "PositionMTB"]):
+                            if mutation_type=="SNP":
+                                mutated=sorted(set(all_res[i].loc[(all_res[i]["MutationType"]==mutation_type) & (all_res[i]["Gene"]==gene) & (all_res[i]["PositionMTB"]==pos), "MutatedAminoAcidOrNucleotide"]))
+                                wildtype=sorted(set(all_res[i].loc[(all_res[i]["MutationType"]==mutation_type) & (all_res[i]["Gene"]==gene) & (all_res[i]["PositionMTB"]==pos), "WildTypeAminoAcidOrNucleotide"]))
+                                common_positions[number][anti][mutation_type].append([i, gene, str(pos), ",".join(wildtype), ",".join(mutated)])
+                            elif mutation_type=="INDEL":
+                                annot=sorted(set(all_res[i].loc[(all_res[i]["MutationType"]==mutation_type) & (all_res[i]["Gene"]==gene) & (all_res[i]["PositionMTB"]==pos), "OriginalAnnotation"]))
+                                common_positions[number][anti][mutation_type].append([i, gene, str(pos), annot])
 
-        
-            
-                
-def alternate_gray_background3(row):
+def alternate_gray_background(row, numb):
     if row.empty:
         return row
     ret=row.copy()
-    ret[[bool(x) for x in row.index//3%2]]='background-color:#DCDCDC'
-    ret[[not bool(x) for x in row.index//3%2]]=''
+    ret[[bool(x) for x in row.index//numb%2]]='background-color:#DCDCDC'
+    ret[[not bool(x) for x in row.index//numb%2]]=''
     return(ret)
   
-                    
-
-                
-def alternate_gray_background(row):
-    if row.empty:
-        return row
-    ret=row.copy()
-    ret[[bool(x) for x in row.index//4%2]]='background-color:#DCDCDC'
-    ret[[not bool(x) for x in row.index//4%2]]=''
-    return(ret)
-  
-writer = pandas.ExcelWriter("summary.xlsx")
-
-for i in sorted(set(list(gene_antibio["Antibiotic"]))):
-    print(i)
-    res_panda_snp=pandas.DataFrame(common_positions[i]["SNP"], columns=["Source", "Gene", "Position", "WildTypeAminoAcidorNucleotide", "MutatedAminoAcidOrNucleotide"])
-
-    #    res_panda_indel=pandas.DataFrame(common_positions["INDEL"], columns=["Source", "Gene", "Position", "OriginalAnnotation"])
-
-
-
-    res_panda_snp.style.apply(alternate_gray_background, axis=None).to_excel(writer, i, index=False)
-writer.save()
+for j in ["4", "3", "2"]:
+    writer = pandas.ExcelWriter("summary_common_to_"+j+"_dbs.xlsx")
+    for i in sorted(set(list(gene_antibio["Antibiotic"]))):
+        res_panda_snp=pandas.DataFrame(common_positions[j][i]["SNP"], columns=["Source", "Gene", "Position", "WildTypeAminoAcidorNucleotide", "MutatedAminoAcidOrNucleotide"])
+        res_panda_snp.style.apply(alternate_gray_background, axis=None, numb=int(j)).to_excel(writer, i, index=False)
+    writer.save()
 #    res_panda_indel.style.apply(alternate_gray_background, axis=None).to_excel(writer, "INDEL", index=False)
-
-
-
-
-  
-writer = pandas.ExcelWriter("summary3.xlsx")
-
-for i in sorted(set(list(gene_antibio["Antibiotic"]))):
-    print(i)
-    res_panda_snp=pandas.DataFrame(common_positions_3[i]["SNP"], columns=["Source", "Gene", "Position", "WildTypeAminoAcidorNucleotide", "MutatedAminoAcidOrNucleotide"])
-
-    #    res_panda_indel=pandas.DataFrame(common_positions["INDEL"], columns=["Source", "Gene", "Position", "OriginalAnnotation"])
-
-
-
-    res_panda_snp.style.apply(alternate_gray_background3, axis=None).to_excel(writer, i, index=False)
-writer.save()
-#    res_panda_indel.style.apply(alternate_gray_background, axis=None).to_excel(writer, "INDEL", index=False)
-
-
-
