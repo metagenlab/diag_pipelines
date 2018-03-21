c7eebba849dad4d5ec0b02acf2227d353d1ea631
Cashalow <sachalau@gmail.com>
2018-03-21 17:55:51 +0100

    now extracting upstream and downstream regions of genes for better mapping

--- rules/annotation/resistance/scripts/get_cds_from_locus_tags.py
+++ rules/annotation/resistance/scripts/get_cds_from_locus_tags.py
@@ -1,7 +1,7 @@
 from Bio import SeqIO
 
-def extract_translation(filename, locus, out):
-    with open(out, "w") as outfile:
+def extract_translation(filename, locus, genes_out, shift):
+    with open(genes_out, "w") as genes:
         for record in SeqIO.parse(filename, "genbank"):
             version=(record.id)
             for f in record.features:
@@ -9,12 +9,12 @@ def extract_translation(filename, locus, out):
                     if locus & set(f.qualifiers["locus_tag"]):
                         loc = locus & set(f.qualifiers["locus_tag"])
                         if f.location.strand == -1:
-                            outfile.write(">"+str(list(loc)[0])+"\n"+str(record.seq[f.location.start:f.location.end].reverse_complement())+"\n")
+                            genes.write(">"+str(list(loc)[0])+"\n"+str(record.seq[f.location.start-shift:f.location.end+shift].reverse_complement())+"\n")
                         else:
-                            outfile.write(">"+str(list(loc)[0])+"\n"+str(record.seq[f.location.start:f.location.end])+"\n")
+                            genes.write(">"+str(list(loc)[0])+"\n"+str(record.seq[f.location.start-shift:f.location.end+shift])+"\n")
 
     
 gbk = snakemake.input["gbk"]
 locus_tags = [x.strip() for x in list(open(snakemake.input["locus_list"]))]
-print(locus_tags)
-extract_translation(gbk, set(locus_tags), snakemake.output["genes"])
+extract_translation(gbk, set(locus_tags), snakemake.output["genes"], snakemake.params["upstream_downstream_size"])
+
