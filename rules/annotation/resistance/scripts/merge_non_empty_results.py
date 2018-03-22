4b6f9525ec252cf873fb25c09317667822e1a5e9
Cashalow <sachalau@gmail.com>
2018-03-22 12:37:52 +0100

    merge non empty results

--- rules/annotation/resistance/scripts/merge_non_empty_results.py
+++ rules/annotation/resistance/scripts/merge_non_empty_results.py
@@ -9,7 +9,7 @@ def natural_keys(text):
     return [ atoi(c) for c in re.split('(\d+)', text) ]
 
 
-snakemake.input.sort(key=natural_keys)
+snakemake.input["xlsx"].sort(key=natural_keys)
 
 out_xlsx = snakemake.output[0]
 
@@ -20,6 +20,8 @@ writer = pandas.ExcelWriter(out_xlsx)
 for j in excels:
     for sheet in j.sheet_names:
         df = j.parse(sheet)
-        df.to_excel(writer, sheet, index=False)
-        writer.save()
+        if df.shape[0]:
+            df.to_excel(writer, sheet, index=False)
+            
+writer.save()
 
