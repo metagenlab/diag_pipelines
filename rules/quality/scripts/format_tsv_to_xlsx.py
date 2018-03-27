fcf0b21b28d73afa533f03f07dfdcb6f7253bbb7
Cashalow <sachalau@gmail.com>
2018-03-27 17:47:46 +0200

    merging contamination result files

--- rules/quality/scripts/format_tsv_to_xlsx.py
+++ rules/quality/scripts/format_tsv_to_xlsx.py
@@ -2,5 +2,5 @@ import pandas
 
 
 writer = pandas.ExcelWriter(snakemake.output["xlsx"])
-pandas.read_csv(snakemake.input["tsv"], sep="\t", header=None, names=["Identity", "SharedHashes", "PValue", "Query"]).to_excel(writer, index=False, sheet_name=snakemake.wildcards["results"])
+pandas.read_csv(snakemake.input["tsv"], sep="\t", header=None, names=["Identity", "SharedHashes", "PValue", "Query"]).to_excel(writer, index=False, sheet_name=snakemake.wildcards["sample"])
 writer.save()
