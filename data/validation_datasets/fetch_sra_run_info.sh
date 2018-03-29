8d806ad963a4c3eba3f92fafbd246ae8b760a760
Cashalow <sachalau@gmail.com>
2018-03-29 17:58:52 +0200

    adding fixes

--- data/validation_datasets/fetch_sra_run_info.sh
+++ data/validation_datasets/fetch_sra_run_info.sh
@@ -6,6 +6,7 @@ do
     ln -s -f ${main}/core_genomes/${genus}_${species%%_*} core_genome
     esearch -db sra -query "${bioproject}[BIOPROJECT] AND \"${genus} ${species}\"[ORGANISM]" < /dev/null | efetch -db sra -format runinfo | sed "s/,/\t/g" | head -n 1 > ${bioproject}_${genus}-${species}.tsv
     esearch -db sra -query "${bioproject}[BIOPROJECT] AND \"${genus} ${species}\"[ORGANISM]" < /dev/null | efetch -db sra -format runinfo |  grep -v "Run,ReleaseDate," | sed "s/,/\t/g" | grep -v "^$" >> ${bioproject}_${genus}-${species}.tsv
-    snakemake --snakefile ${pipeline_folder}/workflows/validation.rules --config sra_samples=${bioproject}_${genus}-${species}.tsv species="${genus} ${species}" absolute_path_of_pipeline_folder=${pipeline_folder} logging_folder="logging/" --configfile ${pipeline_folder}/data/validation_datasets/config.yaml -n -q
+    snakemake --snakefile ${pipeline_folder}/workflows/typing.rules --config sra_samples=${bioproject}_${genus}-${species}.tsv species="${genus} ${species}" --configfile ${pipeline_folder}/data/validation_datasets/config.yaml -n typing/freebayes_joint_genotyping/cgMLST/bwa/distances_in_snp_mst_no_st.svg typing/gatk_gvcfs/cgMLST/bwa/distances_in_snp_mst_no_st.svg phylogeny/freebayes_joint_genotyping/cgMLST/bwa/phylogeny_no_st.svg
+
     cd ..
 done 
