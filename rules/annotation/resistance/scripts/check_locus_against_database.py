71baa40a9513c4772195344e003f62ba1fe57e1f
Cashalow <sachalau@gmail.com>
2018-04-20 18:22:12 +0200

    checking db correctedness directly on the assembly, incorporating tests for the checks

--- rules/annotation/resistance/scripts/check_locus_against_database.py
+++ rules/annotation/resistance/scripts/check_locus_against_database.py
@@ -2,55 +2,58 @@ from Bio import SeqIO
 from Bio import Seq
 import pandas
 
-mutation_entries = pandas.read_csv(snakemake.input["db"], sep="\t")
-correspondance = pandas.read_csv(snakemake.input["gene_to_locus"], sep="\t", index_col=0)
+annotations = pandas.read_csv(snakemake.input["db"], sep="\t")
+gene_to_locustag = pandas.read_csv(snakemake.input["gene_to_locus"], sep="\t", index_col=0)
 
-all_features = {}
+gbk_features = {}
 
 column_names = ["Gene", "PositionMTB", "WildTypeAminoAcidOrNucleotide", "MutatedAminoAcidOrNucleotide", "MutationType"]
-correct = pandas.DataFrame(columns=column_names)
-incorrect = pandas.DataFrame(columns=column_names)
+
+correct_annotations = pandas.DataFrame(columns=column_names)
+incorrect_annotations = pandas.DataFrame(columns=column_names)
 
 for record in SeqIO.parse(snakemake.input["gbk"], "genbank"):
     h37rv = record
     for feat in record.features:
         if feat.type=="gene":
-            all_features[feat.qualifiers["locus_tag"][0]] = feat
+            gbk_features[feat.qualifiers["locus_tag"][0]] = feat
 
-for gene in mutation_entries["Gene"].unique():
-    if correspondance.loc[gene, "LocusTag"] not in all_features.keys():
-        incorrect = incorrect.append(mutation_entries.loc[(mutation_entries["Gene"] == gene), ["Gene", "PositionMTB", "WildTypeAminoAcidOrNucleotide", "MutatedAminoAcidOrNucleotide", "MutationType"]])
+for gene in annotations["Gene"].unique():
+    if gene_to_locustag.loc[gene, "LocusTag"] not in gbk_features.keys():
+        incorrect_annotations = incorrect_annotations.append(annotations.loc[(mutation_entries["Gene"] == gene), ["Gene", "PositionMTB", "WildTypeAminoAcidOrNucleotide", "MutatedAminoAcidOrNucleotide", "MutationType"]])
     else:
-        for index, row in mutation_entries.loc[(mutation_entries["Gene"] == gene) & (mutation_entries["MutationType"] == "SNP")].iterrows():
+        for index, row in annotations.loc[(annotations["Gene"] == gene) & (annotations["MutationType"] == "SNP")].iterrows():
             status = 0
-            coordinates = all_features[correspondance.loc[gene, "LocusTag"]].location
-            #RNA gene checking
-            if gene == "rrs":
-                if coordinates.strand == 1 and record.seq[coordinates.start:coordinates.end][int(row["PositionMTB"])-1].lower() == row["WildTypeAminoAcidOrNucleotide"].lower():
+            coordinates = gbk_features[gene_to_locustag.loc[gene, "LocusTag"]].location
+            #RNA gene checking: no codon
+            position_in_gene = int(row["PositionMTB"])
+            if "rrs" in gene:
+                if coordinates.strand == 1 and record.seq[coordinates.start + position_in_gene - 1].lower() == row["WildTypeAminoAcidOrNucleotide"].lower():
                     status = 1
-                elif coordinates.strand == -1 and Seq.Seq(record.seq[coordinates.end-int(row["PositionMTB"])-1]).reverse_complement().lower() ==  row["WildTypeAminoAcidOrNucleotide"].lower():
+                #THIS IS TESTED WITH E_COLI TEST DB IN THE REPO
+                elif coordinates.strand == -1 and Seq.Seq(record.seq[coordinates.end - position_in_gene]).reverse_complement().lower() ==  row["WildTypeAminoAcidOrNucleotide"].lower():
                     status = 1
-            #Promoter regions checking
-            elif int(row["PositionMTB"]) < 0:
-                if coordinates.strand == 1 and record.seq[coordinates.start+int(row["PositionMTB"])].lower() == row["WildTypeAminoAcidOrNucleotide"].lower():
+            #Promoter regions checking: no codon, negative positions
+            elif position_in_gene < 0:
+                if coordinates.strand == 1 and record.seq[coordinates.start + position_in_gene ].lower() == row["WildTypeAminoAcidOrNucleotide"].lower():
                     status = 1
-                elif coordinates.strand == -1 and Seq.Seq(record.seq[coordinates.end-int(row["PositionMTB"])-1]).reverse_complement().lower() ==  row["WildTypeAminoAcidOrNucleotide"].lower():
+                elif coordinates.strand == -1 and Seq.Seq(record.seq[coordinates.end - position_in_gene -1]).reverse_complement().lower() ==  row["WildTypeAminoAcidOrNucleotide"].lower():
                     status = 1
-            #Mutation leading to codon change checking
+            #The rest is codon checking
             else:
-                if coordinates.strand == 1 and record.seq[coordinates.start:coordinates.end].translate(table=11, cds=True)[int(row["PositionMTB"])-1].lower() == row["WildTypeAminoAcidOrNucleotide"].lower():
+                if coordinates.strand == 1 and record.seq[(coordinates.start + (position_in_gene - 1)*3):(coordinates.start+(position_in_gene * 3))].translate(table=11).lower() == row["WildTypeAminoAcidOrNucleotide"].lower():
                     status = 1 
-                elif coordinates.strand == -1 and record.seq[coordinates.start:coordinates.end].reverse_complement().translate(table=11, cds=True)[int(row["PositionMTB"])-1].lower() == row["WildTypeAminoAcidOrNucleotide"].lower():
+                elif coordinates.strand == -1 and record.seq[(coordinates.end - position_in_gene * 3):(coordinates.end - (position_in_gene - 1)*3)].reverse_complement().translate(table=11).lower() == row["WildTypeAminoAcidOrNucleotide"].lower():
                     status = 1
             if status:
-                correct = correct.append(pandas.DataFrame([[gene, row["PositionMTB"], row["WildTypeAminoAcidOrNucleotide"], row["MutatedAminoAcidOrNucleotide"], row["MutationType"]]], columns=column_names))
+                correct_annotations = correct_annotations.append(pandas.DataFrame([[gene, row["PositionMTB"], row["WildTypeAminoAcidOrNucleotide"], row["MutatedAminoAcidOrNucleotide"], row["MutationType"]]], columns=column_names))
             else:
-                incorrect = incorrect.append(pandas.DataFrame([[gene, row["PositionMTB"], row["WildTypeAminoAcidOrNucleotide"], row["MutatedAminoAcidOrNucleotide"], row["MutationType"]]], columns=column_names))
+                incorrect_annotations = incorrect_annotations.append(pandas.DataFrame([[gene, row["PositionMTB"], row["WildTypeAminoAcidOrNucleotide"], row["MutatedAminoAcidOrNucleotide"], row["MutationType"]]], columns=column_names))
                 
         
                                     
-correct.to_csv(snakemake.output["correct_annotation"], sep="\t", index=False)
-incorrect.to_csv(snakemake.output["incorrect_annotation"], sep="\t", index=False)
+correct_annotations.to_csv(snakemake.output["correct_annotation"], sep="\t", index=False)
+incorrect_annotations.to_csv(snakemake.output["incorrect_annotation"], sep="\t", index=False)
 
                                 
 
