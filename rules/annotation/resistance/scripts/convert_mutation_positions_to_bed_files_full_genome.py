7112875f2593567486eac07e27137ed351abd3e4
Cashalow <sachalau@gmail.com>
2018-04-20 18:22:42 +0200

    starting working on bed file generation

--- rules/annotation/resistance/scripts/convert_mutation_positions_to_bed_files_full_genome.py
+++ rules/annotation/resistance/scripts/convert_mutation_positions_to_bed_files_full_genome.py
@@ -9,16 +9,33 @@ for record in SeqIO.parse(snakemake.input["gbk"], "genbank"):
             all_features[feat.qualifiers["locus_tag"][0]] = feat
 
 
-
 mutations = pandas.read_csv(snakemake.input["db_correct"], sep="\t", index_col=0)
 correspondance = pandas.read_csv(snakemake.input["gene_to_locus"], sep="\t", index_col=0)
 
+print(mutations)
+
+
+
 for index, row in mutations.join(correspondance).iterrows():
     coordinates = all_features[row["LocusTag"]].location
-    if index != "rrs" and row["PositionMTB"] > 0:
+    if index == "rrs":
         if coordinates.strand == 1:
-            index, coordinates)
-
+            loc = (coordinates.start + row["PositionMTB"], coordinates.start + row["PositionMTB"] + 1)
+        elif coordinates.strand == -1:
+            loc = (coordinates.end - row["PositionMTB"], coordinates.end - row["PositionMTB"] - 1)
+        print(loc)
+    elif int(row["PositionMTB"]) < 0:
+        if coordinates.strand == 1:
+            loc = (coordinates.start + row["PositionMTB"], coordinates.start + row["PositionMTB"] + 1)
+        elif coordinates.strand == -1:
+            loc = (coordinates.end - row["PositionMTB"], coordinates.end - row["PositionMTB"] + 1)
+        print(loc)
+    else:
+        if coordinates.strand == 1:
+            loc = (coordinates.start + (row["PositionMTB"]-1)*3, coordinates.start + row["PositionMTB"]*3)
+        elif coordinates.strand == -1:
+            loc = (coordinates.end - row["PositionMTB"]*3, coordinates.end - (row["PositionMTB"]-1)*3)
+    print(row, loc)
     
 
 
