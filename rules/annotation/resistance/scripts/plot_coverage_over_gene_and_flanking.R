6375b8c69d13a7f5e08691fb80a995cfd12f1bda
Cashalow <sachalau@gmail.com>
2018-04-26 20:02:08 +0200

    clearer script

--- rules/annotation/resistance/scripts/plot_coverage_over_gene_and_flanking.R
+++ rules/annotation/resistance/scripts/plot_coverage_over_gene_and_flanking.R
@@ -1,16 +1,14 @@
 library(svglite)
 
-data <- read.csv(snakemake@input[["coverage_txt"]], sep="\t", header=FALSE, stringsAsFactor=FALSE)
+data <- read.csv(snakemake@input[["coverage_txt"]], sep="\t", header=FALSE, stringsAsFactor=FALSE, col.names = c("Reference", "Start", "End", "Strand", "Position", "Coverage"))
 
-strand <- data[1, 4]
-
-print(head(data))
+strand <- data[1, "Strand"]
 
 if(strand == -1) {title <- paste(snakemake@wildcards[["gene"]], "(negative strand)")
 } else title <- paste(snakemake@wildcards[["gene"]], "(positive strand)")
 
 svg(snakemake@output[["coverage_plot"]])
-plot(data$V2+data$V5, data$V6, type="l", xlab=paste("Position on", snakemake@wildcards[["ref"]], "assembly"), ylab="Coverage", main = title)
-rect(data$V2[1]+snakemake@params[["up_down"]], 0, data$V3[1]-snakemake@params[["up_down"]], max(data$V6), col=rgb(96/256,168/256,98/256, alpha=0.5), border=NA)
+plot(data[,"Start"]+data[,"Position"], data[,"Coverage"], type="l", xlab=paste("Position on", snakemake@wildcards[["ref"]], "assembly"), ylab="Coverage", main = title)
+rect(data[1, "Start"]+snakemake@params[["up_down"]], 0, data[1, "End"]-snakemake@params[["up_down"]], max(data[,"Coverage"]), col=rgb(96/256,168/256,98/256, alpha=0.5), border=NA)
 graphics.off()
 
