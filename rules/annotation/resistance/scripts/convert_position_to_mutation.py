d693b79430b3556a10b12f79db785df8359d8297
Cashalow <sachalau@gmail.com>
2018-04-27 15:49:44 +0200

    script for getting the mutated amino acid or nucleotide

---
+++ rules/annotation/resistance/scripts/convert_position_to_mutation.py
@@ -0,0 +1,27 @@
+from Bio import SeqIO, Seq
+
+import pandas
+
+genotype = pandas.read_csv(snakemake.input["genotype"], sep=" ", names=["Chrom", "Position", "Genotype", "Strand"])
+
+mutation = []
+
+for index, row in genotype.iterrows():
+    if len(row["Genotype"])==3:
+        if row["Strand"]==1:
+            mutation.append(str(Seq.Seq(row["Genotype"]).translate(table=11)))
+        elif row["Strand"]==-1:
+            mutation.append(str(Seq.Seq(row["Genotype"]).reverse_complement().translate(table=11)))
+    elif len(row["Genotype"])==1:
+        if row["Strand"]==1:
+            mutation.append(row["Genotype"])
+        elif row["Strand"]==-1:
+            mutation.append(str(Seq.Seq(row["Genotype"]).reverse_complement()))
+
+#we switch to bed file coordinates, which are 0 based
+
+genotype["Start"] = genotype["Position"] - 1
+genotype["End"] = genotype["Position"]
+
+pandas.concat([genotype[["Chrom", "Start", "End"]], pandas.Series(mutation)], axis=1).to_csv(snakemake.output["mutation"], sep="\t", index=False, header=False)
+
