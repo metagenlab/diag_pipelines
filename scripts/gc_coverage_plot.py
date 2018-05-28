000d6437986e27d2f81de59e783497a50d402f11
Trestan Pillonel <trestan.pillonel@gmail.com>
2018-05-28 20:23:39 +0200

    gc_cov_plot script: additional plot without extremely high depth

--- scripts/gc_coverage_plot.py
+++ scripts/gc_coverage_plot.py
@@ -141,19 +141,20 @@ def gc_coverage_plot(contigs_file,
         gc_coverage_table <-cbind(contigs_gc,coverage=contigs_depth[match(contigs_gc$Name, contigs_depth$contig),2])
         #w<-which(gc_coverage_table$Length >=1000)
         #gc_coverage_table <- gc_coverage_table[w,]
+
+         cov_biggest <- gc_coverage_table[which(gc_coverage_table$Length==max(gc_coverage_table$Length)),4]
+         #print('cov biggest:')
+         #print(cov_biggest)
+         w <- which(gc_coverage_table[,4]< (4*cov_biggest))
+         gc_coverage_table_2m <- gc_coverage_table[w,]
     
         if (contig_labels != FALSE) {
             library(RColorBrewer)
             color_palette <- c('red', 'blue','green', brewer.pal(12,"Paired"), brewer.pal(12,"Set3"))
             m <- match(contig_labels$contig, gc_coverage_table$Name)
 
-            #print(contig_labels$contig)
-            #print(gc_coverage_table$Name)
-
             gc_coverage_table$color <- rep("Unclassified", length(gc_coverage_table[,1]))
             gc_coverage_table$contig_alpha <- rep(0.5, length(gc_coverage_table[,1]))
-
-            #print(m)
         
             gc_coverage_table$color[m] <- as.character(contig_labels$label)
             gc_coverage_table$contig_alpha[m] <- rep(1,length(contig_labels$label))
@@ -163,8 +164,6 @@ def gc_coverage_plot(contigs_file,
             #w2 <- which(gc_coverage_table$color != "Chlamydiae")
             #gc_coverage_table$contig_alpha[w2] <- 0.7
 
-            #print(gc_coverage_table)
-        
             svg("%sgc_cov_buble_test.svg", width = 12, height = 12)
             p6 <- ggplot(gc_coverage_table, aes(x = X.GC, y = coverage, size = Length, fill = color, colour = color, alpha = contig_alpha)) +
                     geom_point(shape = 21) +
@@ -176,8 +175,30 @@ def gc_coverage_plot(contigs_file,
             #print (max(gc_coverage_table$Length))
             p6 <- p6 + scale_alpha_continuous(range=c(0.1, 1), limits=c(0.1,1)) #+ scale_alpha_continuous(range=c(0, max(gc_coverage_table$Length)), limits=c(0,max(gc_coverage_table$Length)))
             
-            #print(p6 + theme_bw())
+            print(p6 + theme_bw())
             dev.off()
+
+            gc_coverage_table_2m$color <- rep("Unclassified", length(gc_coverage_table_2m[,1]))
+            gc_coverage_table_2m$contig_alpha <- rep(0.5, length(gc_coverage_table_2m[,1]))
+        
+            gc_coverage_table_2m$color[m] <- as.character(contig_labels$label)
+            gc_coverage_table_2m$contig_alpha[m] <- rep(1,length(contig_labels$label))
+
+            svg("%sgc_cov_buble_test_2m.svg", width = 12, height = 12)
+            p6 <- ggplot(gc_coverage_table_2m, aes(x = X.GC, y = coverage, size = Length, fill = color, colour = color, alpha = contig_alpha)) +
+                    geom_point(shape = 21) +
+                    ggtitle("Scaffold GC vs Depth") +
+                    labs(x = "GC (%%)", y = "Sequencing depth") +
+                    scale_size(range = c(1, 10))
+            p6 <- p6 + scale_fill_manual(values=color_palette[0:length(unique(gc_coverage_table$color))])+ guides(color = guide_legend(override.aes = list(size=5)))
+            p6 <- p6 + scale_colour_manual(values=color_palette[0:length(unique(gc_coverage_table$color))])
+            #print (max(gc_coverage_table$Length))
+            p6 <- p6 + scale_alpha_continuous(range=c(0.1, 1), limits=c(0.1,1)) #+ scale_alpha_continuous(range=c(0, max(gc_coverage_table$Length)), limits=c(0,max(gc_coverage_table$Length)))
+            
+            print(p6 + theme_bw())
+            dev.off()
+
+
             
         }else{
             #print('NO contig_labels')
@@ -202,11 +223,7 @@ def gc_coverage_plot(contigs_file,
 
          dev.off()
 
-         cov_biggest <- gc_coverage_table[which(gc_coverage_table$Length==max(gc_coverage_table$Length)),4]
-         #print('cov biggest:')
-         #print(cov_biggest)
-         w <- which(gc_coverage_table[,4]< (4*cov_biggest))
-         gc_coverage_table_2m <- gc_coverage_table[w,]
+
 
 
          %s
@@ -233,12 +250,13 @@ def gc_coverage_plot(contigs_file,
                           samtool_depth_file,
                           samtool_depth_file,
                           output_prefix,
+                          output_prefix,
                           highlight_code,
                           output_prefix,
                           main,
                           highlight_code2,
-                          main,
-                          output_prefix))
+                          output_prefix,
+                          main))
     else:
 
         robjects.r("""
