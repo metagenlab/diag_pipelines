3a86ec2ed30b708005cbc57547441a3edc05a508
Cashalow <sachalau@gmail.com>
2018-05-31 15:07:35 +0200

    pandas

--- rules/annotation/resistance/scripts/fetch_ontology_rgi.py
+++ rules/annotation/resistance/scripts/fetch_ontology_rgi.py
@@ -11,10 +11,12 @@ mo = pronto.Ontology(snakemake.params["ontology_mo"])
 aro.merge(ro)
 aro.merge(mo)
 
+ontology_results = []
 
-def extract_rgi(row, aro_ont, gene_list):
-    res = []
-    if row["SNP"] != "n/a":
+rgi_results = pandas.read_csv(snakemake.input['rgi'], sep="\t")
+
+for index, row in rgi_results.iterrows():
+    if row["Model_type"] == "protein variant model":
         pass
 #        best_hit = row["Best_Hit_ARO"].split()
 #        genes = list(set(best_hit).intersection(set(gene_list)))
@@ -35,32 +37,24 @@ def extract_rgi(row, aro_ont, gene_list):
 #                            res.append(["rgi", gene, "variant model", ref+str(pos)+mut, anti])
 #        else:
 #            raise Exception("Problem parsing the predicted ARO Model from RGI")
-    elif "intrinsic" not in row["Best_Hit_ARO"]:
+    elif row["Model_type"] == "protein homolog model":
         for terms in row["ARO"].split(","):
-            term = aro_ont[terms.strip()]
+            term = aro[terms.strip()]
             gene = row["Best_Hit_ARO"].replace(" ", "_").strip()
             for child in term.relations:
                 if child.obo_name=="confers_resistance_to_drug" or  child.obo_name=="confers_resistance_to":
                     for antibiotic in term.relations[child]:
                         anti = antibiotic.name.replace("antibiotic", "").strip()
-                        res.append(["rgi", gene, "homology model", "", anti])
+                        ontology_results.append(["rgi", gene, "homology model", "", anti])
             for parent in term.rparents():
                 for child in parent.relations:
                     if child.obo_name=="confers_resistance_to_drug" or  child.obo_name=="confers_resistance_to":
                         for antibiotic in parent.relations[child]:
                             anti = antibiotic.name.replace("antibiotic", "").strip()
-                            res.append(["rgi", gene, "homology model", "", anti])
-    return(res)
-
-all_res = []
+                            ontology_results.append(["rgi", gene, "homology model", "", anti])
 
 
-with open(snakemake.input["rgi"]) as tsvfile:
-    csvreader = csv.DictReader(tsvfile, delimiter="\t")
-    for row in csvreader:
-        all_res += extract_rgi(row, aro, [])
-            
-df = pandas.DataFrame(all_res, columns= ["Software", "Gene", "Resistance Type", "Variant", "Antibiotic resistance prediction"])
+df = pandas.DataFrame(ontology_results, columns= ["Software", "Gene", "Resistance Type", "Variant", "Antibiotic resistance prediction"])
 
 
 writer = pandas.ExcelWriter(snakemake.output["xlsx"])
