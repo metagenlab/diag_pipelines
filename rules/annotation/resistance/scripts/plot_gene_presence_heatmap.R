648c99718a3e81deec2abc37ff1d31fd74dd3089
tpillone <trestan.pillonel@gmail.com>
2018-07-03 13:15:59 +0200

    new rule: plot heatmap resistanc presence/absencle from multiple samples

---
+++ rules/annotation/resistance/scripts/plot_gene_presence_heatmap.R
@@ -0,0 +1,72 @@
+library(ggplot2)
+library(gridExtra)
+
+rgi_files <- snakemake@input[["rgi_files"]]
+species <- snakemake@params[["species"]]
+mechanism <- snakemake@input[["rgi_mechanism_files"]]
+print(mechanism)
+sample_table <- data.frame(file=rgi_files, species=species,mechanism=mechanism, stringsAsFactors=FALSE)
+
+for (i in 1:nrow(sample_table)){
+  rgi_results_file <- sample_table[i,1]
+  mechanism_file <-sample_table[i,3]
+  species <- sample_table[i,2]
+
+  # if the merged dataset doesn't exist, create it
+  if (!exists("dataset")){
+    dataset <- read.csv(rgi_results_file, header=TRUE, sep="\t",stringsAsFactors=FALSE)
+    dataset$sample <- rep(unlist(strsplit(rgi_results_file, "/"))[2], length(dataset[,1]))
+    dataset$species <- rep(species, length(dataset[,1]))
+    resistance_mechanism <-  read.csv(mechanism_file, header=TRUE, sep="\t",stringsAsFactors=FALSE)
+    m <- match(dataset$Best_Hit_ARO, resistance_mechanism$Gene)
+    dataset$mechanism <- resistance_mechanism$Resistance.Mechanism[m]
+
+
+  }
+  # if the merged dataset does exist, append to it
+  if (exists("dataset")){
+    temp_dataset <-read.csv(rgi_results_file, header=TRUE, sep="\t",stringsAsFactors=FALSE)
+    temp_dataset$sample <- rep(unlist(strsplit(rgi_results_file, "/"))[2], length(temp_dataset[,1]))
+    temp_dataset$species <- rep(species, length(temp_dataset[,1]))
+    resistance_mechanism <-  read.csv(mechanism_file, header=TRUE, sep="\t",stringsAsFactors=FALSE)
+    m <- match(temp_dataset$Best_Hit_ARO, resistance_mechanism$Gene)
+    temp_dataset$mechanism <- resistance_mechanism$Resistance.Mechanism[m]
+
+    dataset<-rbind(dataset, temp_dataset)
+    rm(temp_dataset)
+
+  }
+}
+
+u <- unique(dataset$Best_Hit_ARO)
+u_sort <- u[rev(order(u))]
+dataset$Best_Hit_ARO <- factor(dataset$Best_Hit_ARO, levels=u_sort)
+
+pdf(snakemake@output[["rgi_plot"]], height=25,width=0.5*length(rgi_files))
+p <- ggplot(data = dataset, aes(x = sample, y = Best_Hit_ARO)) + geom_raster(aes(fill = CUT_OFF))
+p <- p + theme_grey(base_size = 10)  + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+print (p + coord_fixed(ratio=1))
+dev.off()
+
+###
+# produce one plot per species
+###
+
+nr_species <- unique(sample_table$species)
+if (length(nr_species) > 1){
+    for (species in nr_species){
+        sub_dataset <- dataset[dataset$species==species,]
+        sub_dataset$Best_Hit_ARO <- as.character(sub_dataset$Best_Hit_ARO)
+        u <- unique(sub_dataset$Best_Hit_ARO)
+        u_sort <- u[rev(order(u))]
+        sub_dataset$Best_Hit_ARO <- factor(sub_dataset$Best_Hit_ARO, levels=u_sort)
+        w <- length(unique(sub_dataset$species))*6
+        h <- length(unique(sub_dataset$Best_Hit_ARO))/4
+        pdf(paste('resistance/', species, '.pdf', sep=''), width=w, height=h)
+        print(species)
+        p <- ggplot(data = sub_dataset, aes(x = sample, y = Best_Hit_ARO)) + geom_tile(aes(fill = CUT_OFF), height = 0.9, width=0.9)
+        p <- p + theme_grey(base_size = 10)  + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+        print(p + coord_fixed(ratio=1) + facet_grid(. ~ mechanism, switch = "y")) + theme(strip.text.x = element_text(size=8, angle=75))
+        dev.off()
+    }
+}
---
+++ rules/annotation/resistance/scripts/rgi_get_resistance_mechanism.py
@@ -0,0 +1,75 @@
+import pandas
+import pronto
+import csv
+import re
+from pronto import Relationship
+
+aro = pronto.Ontology(snakemake.params["ontology_aro"])
+ro = pronto.Ontology(snakemake.params["ontology_ro"])
+mo = pronto.Ontology(snakemake.params["ontology_mo"])
+cat = snakemake.params["aro_categories"]
+categories = pandas.read_csv(cat, sep="\t")
+
+resistance_mechanism = categories.loc[categories['ARO Category'] == 'Resistance Mechanism']
+
+obo2resistance_mechanism = pandas.Series(resistance_mechanism['ARO Name'].values, index=resistance_mechanism['ARO Accession']).to_dict()
+
+aro.merge(ro)
+aro.merge(mo)
+
+ontology_results = []
+
+rgi_results = pandas.read_csv(snakemake.input['rgi'], sep="\t")
+
+
+def get_all_path(obo_list, all_obo):
+    new_obo = []
+    for obo in obo_list:
+        new_obo+=obo.parents
+        all_obo+=obo.parents
+    if len(new_obo) > 0:
+        return get_all_path(new_obo, all_obo=all_obo)
+    else:
+        return all_obo
+
+def search_participating_mechanism(obo_term, mechanism_list):
+    for relation in obo_term.relations:
+        print ("relation level 1",relation, obo_term.relations[relation])
+        if relation.obo_name=="participates_in":
+            if obo_term.relations[relation][0].id in mechanism_list:
+                return obo_term.relations[relation][0]
+            else:
+                all_path = get_all_path(obo_term.relations[relation], [])
+                try:
+                    return [i for i in all_path if i.id in mechanism_list][0]
+                except IndexError:
+                    continue
+        elif relation.obo_name in ["regulates"]:
+            # need a fix
+            print ('Regulator')
+            relation.name = 'regulator'
+            return (relation)
+
+term_nr_list = []
+for index, row in rgi_results.iterrows():
+    gene = row["Best_Hit_ARO"]
+
+    for terms in row["ARO"].split(","):
+        term = aro[terms.strip()]
+        for i in term.rparents():
+            mechanism = search_participating_mechanism(i, obo2resistance_mechanism.keys())
+            if mechanism:
+                break
+        # remove redundancy
+        if [gene, mechanism.name] not in term_nr_list:
+            term_nr_list.append([gene, mechanism.name])
+
+df = pandas.DataFrame(term_nr_list, columns= ["Gene", "Resistance Mechanism"])
+
+
+writer = pandas.ExcelWriter(snakemake.output["xlsx"])
+df.drop_duplicates().to_excel(writer, snakemake.wildcards["sample"], index=False)
+writer.save()
+
+df.drop_duplicates().to_csv(snakemake.output["tsv"], index= False, sep="\t")
+writer.save()
