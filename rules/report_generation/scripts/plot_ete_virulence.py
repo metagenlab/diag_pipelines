f7a2bdfb1e8aa2fbbf472296315ff1ef6ccb1498
tpillone <trestan.pillonel@gmail.com>
2018-07-05 19:02:28 +0200

    added mecA identification to s aureus overview figure

--- rules/report_generation/scripts/plot_ete_virulence.py
+++ rules/report_generation/scripts/plot_ete_virulence.py
@@ -13,8 +13,11 @@ m_red = cm.ScalarMappable(norm=norm, cmap=cmap_red)
 m_blue = cm.ScalarMappable(norm=norm, cmap=cmap_blue)
 m_green = cm.ScalarMappable(norm=norm, cmap=cmap_green)
 
-tab_files = [pandas.read_csv(name, delimiter='\t') for name in snakemake.input["nr_blast_results"]]
+blast_files = [pandas.read_csv(name, delimiter='\t') for name in snakemake.input["nr_blast_results"]]
 best_tree = snakemake.input["best_tree"]
+
+ordered_samples = snakemake.params["samples"]
+
 leaf2spa_typing= pandas.read_csv(snakemake.input["spa_typing"],
                                  delimiter='\t',
                                  names=["SAMPLENAME","EGENOMICS_SPA_TYPE", "RIDOM_SPA_TYPE"]).set_index("SAMPLENAME").to_dict()["RIDOM_SPA_TYPE"]
@@ -22,24 +25,48 @@ leaf2spa_typing= pandas.read_csv(snakemake.input["spa_typing"],
 leaf2mlst= pandas.read_csv(snakemake.input["mlst"],
                            delimiter='\t',
                            names=["leaf","spacies","mlst","1","2","3","4","5","6","7"]).set_index("leaf").to_dict()["mlst"]
+
 vf_table = pandas.read_csv(snakemake.input["vf_table"], delimiter='\t')
 vf_list = vf_table["gene"] + '_' + vf_table["uniprot_accession"]
 
+virulence_tables = [pandas.read_csv(name, delimiter='\t') for name in snakemake.input["resistance_tables"]]
+
+leaf_id2meca = {}
+for sample_name, virulence_table in zip(ordered_samples, virulence_tables):
+    try:
+        # attention, could match multiple mecA?
+        CUT_OFF = virulence_table.loc[virulence_table['Best_Hit_ARO'] == 'mecA', 'CUT_OFF'].iloc[0]
+        leaf_id2meca[sample_name] = CUT_OFF
+    except IndexError:
+        leaf_id2meca[sample_name] = '-'
+
 leaf_id2protein_id2identity = {}
-for blast_table, blast_file in zip(tab_files, snakemake.input["nr_blast_results"]):
-    leaf_id = blast_file.split('/')[1]
+for sample_name, blast_table in zip(ordered_samples, blast_files):
     protein_id2identity = blast_table.set_index("virulence_factor_ID").to_dict()["percentage_identity"]
-    leaf_id2protein_id2identity[leaf_id]= protein_id2identity
+    leaf_id2protein_id2identity[sample_name]= protein_id2identity
+
+
+def get_spaced_colors(n):
+
+    max_value = 16581375 #255**3
+    interval = int(max_value / n)
+    colors = [hex(I)[2:].zfill(6) for I in range(0, max_value, interval)]
+
+    return ['#%02x%02x%02x' % (int(i[:2], 16), int(i[2:4], 16), int(i[4:], 16)) for i in colors]
+
 
 
 def plot_ete_tree(tree_file,
                   ordered_queries,
                   leaf_id2protein_id2identity,
-                  leaf_id2description=False,
-                  leaf_id2mlst=False,
-                  leaf_id2spa=False,
-                  show_identity_values=True):
-
+                  leaf_id2mlst,
+                  leaf_id2spa,
+                  leaf_id2meca,
+                  show_identity_values=True,
+                  leaf_id2description=False):
+    mlst_list = list(set(leaf_id2mlst.values()))
+    mlst2color = dict(zip(mlst_list, get_spaced_colors(len(mlst_list))))
+    mlst2color['-'] = 'white'
 
     t1 = Tree(tree_file)
     tss = TreeStyle()
@@ -48,62 +75,107 @@ def plot_ete_tree(tree_file,
     t1.ladderize()
 
     head = True
-    column_add = 0
+    column_add = 4
     for lf in t1.iter_leaves():
         lf.branch_vertical_margin = 0
         # add MLST
-        if leaf_id2mlst:
-            if head:
-                column_add+=1
-                n = TextFace(' MLST ')
-                n.margin_top = 2
-                n.margin_right = 2
-                n.margin_left = 2
-                n.margin_bottom = 2
-                n.rotation = 270
-                n.vt_align = 2
-                n.hz_align = 2
-                n.inner_background.color = "white"
-                n.opacity = 1.
-                tss.aligned_header.add_face(n, 0)
+        if head:
+            n = TextFace(' MLST ')
+            n.margin_top = 2
+            n.margin_right = 2
+            n.margin_left = 2
+            n.margin_bottom = 2
+            n.rotation = 270
+            n.vt_align = 2
+            n.hz_align = 2
+            n.inner_background.color = "white"
+            n.opacity = 1.
+            tss.aligned_header.add_face(n, 1)
 
-            if lf.name in leaf2mlst:
-                n = TextFace(' %s ' % leaf2mlst[lf.name])
-            else:
-                n = TextFace(' na ')
-            n.opacity = 1.
-            n.margin_top = 2
-            n.margin_right = 2
-            n.margin_left = 2
-            n.margin_bottom = 2
+        if lf.name in leaf2mlst:
+            n = TextFace(' %s ' % leaf_id2mlst[lf.name])
+            n.inner_background.color = 'white'
+            m = TextFace('  ')
+            m.inner_background.color = mlst2color[leaf_id2mlst[lf.name]]
+        else:
+            n = TextFace(' na ')
             n.inner_background.color = "grey"
-            lf.add_face(n, 0, position="aligned")
+            m = TextFace('    ')
+            m.inner_background.color = "white"
+
+        n.opacity = 1.
+        n.margin_top = 2
+        n.margin_right = 2
+        n.margin_left = 0
+        n.margin_bottom = 2
+
+        m.margin_top = 2
+        m.margin_right = 0
+        m.margin_left = 20
+        m.margin_bottom = 2
+
+        lf.add_face(m, 0, position="aligned")
+        lf.add_face(n, 1, position="aligned")
+
         # add spa typing
-        if leaf_id2spa:
-            if head:
-                column_add+=1
-                n = TextFace(' spa ')
-                n.margin_top = 2
-                n.margin_right = 2
-                n.margin_left = 2
-                n.margin_bottom = 2
-                n.rotation = 270
-                n.vt_align = 2
-                n.hz_align = 2
-                n.inner_background.color = "white"
-                n.opacity = 1.
-                tss.aligned_header.add_face(n, column_add-1)
-            if lf.name in leaf_id2spa:
-                n = TextFace(' %s ' % leaf_id2spa[lf.name])
-            else:
-                n = TextFace(' na ')
-            n.opacity = 1.
+        if head:
+            n = TextFace(' spa ')
             n.margin_top = 2
             n.margin_right = 2
             n.margin_left = 2
             n.margin_bottom = 2
+            n.rotation = 270
+            n.vt_align = 2
+            n.hz_align = 2
+            n.inner_background.color = "white"
+            n.opacity = 1.
+            tss.aligned_header.add_face(n, column_add-2)
+        if lf.name in leaf_id2spa:
+            n = TextFace(' %s ' % leaf_id2spa[lf.name])
+            n.inner_background.color = "white"
+        else:
+            n = TextFace('  na  ')
             n.inner_background.color = "grey"
-            lf.add_face(n, column_add-1, position="aligned")
+        n.opacity = 1.
+        n.margin_top = 2
+        n.margin_right = 2
+        n.margin_left = 2
+        n.margin_bottom = 2
+
+        lf.add_face(n, column_add-2, position="aligned")
+
+        # add mecA typing
+        if head:
+            n = TextFace(' mecA ')
+            n.margin_top = 2
+            n.margin_right = 2
+            n.margin_left = 2
+            n.margin_bottom = 2
+            n.rotation = 270
+            n.vt_align = 2
+            n.hz_align = 2
+            n.inner_background.color = "white"
+            n.opacity = 1.
+            tss.aligned_header.add_face(n, column_add-1)
+        if lf.name in leaf_id2meca:
+            n = TextFace(' %s ' % leaf_id2meca[lf.name])
+            if leaf_id2meca[lf.name] == 'Perfect':
+                n.inner_background.color = "red"
+            elif leaf_id2meca[lf.name] == 'Strict':
+                n.inner_background.color = "orange"
+            else:
+                n.inner_background.color = "white"
+        else:
+            n = TextFace('   na   ')
+            n.inner_background.color = "grey"
+        n.opacity = 1.
+        n.margin_top = 2
+        n.margin_right = 2
+        n.margin_left = 2
+        n.margin_bottom = 2
+
+        lf.add_face(n, column_add-1, position="aligned")
+
         # loop to add virulence gene hits
         for column, protein_id in enumerate(ordered_queries):
             # draw labels at the top of each column
@@ -135,7 +207,7 @@ def plot_ete_tree(tree_file,
                     tss.aligned_header.add_face(n, column+column_add)
             # draw column content
             if lf.name not in leaf_id2protein_id2identity:
-                n = TextFace(' %s ' % str('   -   '))
+                n = TextFace(' %s ' % str('  na  '))
                 n.opacity = 1.
                 n.margin_top = 2
                 n.margin_right = 2
@@ -190,7 +262,7 @@ def plot_ete_tree(tree_file,
         # end of first leaf: turn off header
         head = False
 
-    # draw boostrap supports
+    # add boostrap supports
     for n in t1.traverse():
         nstyle = NodeStyle()
         if n.support < 0.9:
@@ -208,7 +280,8 @@ t1, tss = plot_ete_tree(best_tree,
               vf_list,
               leaf_id2protein_id2identity,
               leaf_id2spa=leaf2spa_typing,
-              leaf_id2mlst=leaf2mlst)
+              leaf_id2mlst=leaf2mlst,
+              leaf_id2meca=leaf_id2meca)
 
 output_svg = snakemake.output[0]
 t1.render(output_svg, dpi=1000, h=400, tree_style=tss)
