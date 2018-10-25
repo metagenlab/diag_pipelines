0ba6f1b58a44d6f0668c43016d1bd6bfe97075db
Trestan Pillonel <trestan.pillonel@gmail.com>
2018-10-25 11:16:57 +0000

    now use pandas.to_html() to generate VFDB html tables

--- rules/annotation/scripts/generate_VFDB_html_report.py
+++ rules/annotation/scripts/generate_VFDB_html_report.py
@@ -1,96 +1,10 @@
 
+import pandas
+
 blast_result = snakemake.input[0]
 vfdb_annotation = snakemake.input[1]
 sample = snakemake.params[0]
-
-vf_accession2annotation = {}
-with open(vfdb_annotation, 'r') as f:
-    for row in f:
-        data = row.split('\t')
-        vf_accession2annotation[data[0]] = data[1:]
-protein_id2blast_hit = {}
-with open(blast_result, 'r') as f:
-    for row in f:
-        data = row.split('\t')
-        if data[0] in ['ORF_ID', 'virulence_factor_ID']:
-            continue
-        protein_id2blast_hit[data[0]] = data[1:]
-
 report_file = snakemake.output[0]
-
-template = ''''
-<!DOCTYPE html>
-
-
-<html>
-    <head>        
-        
-        <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
-        
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-        
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
-        <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    
-    </head>
-<body>
-    <div id='vftable' style="max-width:90%%; padding-left:35;">
-        <h1>Virulence Factors of sample: %s</h1>
-        <table class="display dataTable" id="VF_table">
-            <thead>
-                <tr>
-                    <th scope="col">ORF ID</th></th>
-                  <th scope="col">Hit</th>
-                  <th scope="col">Virulence Factor</th>
-                  <th scope="col">Link conservation</th>
-                  <th scope="col">Product</th>
-                  <th scope="col">Organism</th>
-                  <th scope="col">Identity (%%)</th>
-                  <th scope="col">e-value</th>
-                  <th scope="col">bit-score</th>
-            </thead>
-            <tbody>
-                %s
-            </tbody>
-        </table>
-    </div>
-
-</body>
-
-<script>
-
-$(document).ready(function() {
-    $('#VF_table').DataTable( {
-        dom: 'Bfrtip',
-        "pageLength": 20,
-        "searching": true,
-        "bLengthChange": false,
-        "paging":   true,
-        "info": false
-    } );
-} );
-</script>
-
-
-'''
-
-row_template = '''
-    <tr>
-        <td>%s</td>
-        <td><a href="http://www.mgc.ac.cn/cgi-bin/VFs/gene.cgi?GeneID=%s">%s</a></td>
-        <td><a href=http://www.mgc.ac.cn/cgi-bin/VFs/vfs.cgi?VFID=%s#%s>%s</a> (%s)</td>
-        <td><a href=https://www.vfdb.chlamdb.ch/VF_VFDB/%s>Link</a></td>
-        <td>%s</td>
-        <td>%s</td>
-        <td>%s</td>
-        <td>%s</td>
-        <td>%s</td>
-    </tr>
-'''
 
 # ORF_ID
 # 0 matching_sequence
@@ -116,24 +30,106 @@ row_template = '''
 # 4 product
 # 5 gbk_accession
 
-table_rows = ''
-for blast_hit in protein_id2blast_hit:
-    vf_accession = protein_id2blast_hit[blast_hit][0]
-    annotation = vf_accession2annotation[vf_accession]
-    table_rows+=row_template % (blast_hit,
-                                protein_id2blast_hit[blast_hit][0],
-                                protein_id2blast_hit[blast_hit][0],
-                                annotation[0],
-                                annotation[0],
-                                annotation[0],
-                                annotation[3],
-                                protein_id2blast_hit[blast_hit][0],
-                                annotation[4],
-                                annotation[2],
-                                protein_id2blast_hit[blast_hit][1],
-                                protein_id2blast_hit[blast_hit][9],
-                                protein_id2blast_hit[blast_hit][10],)
+
+STYLE = """
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">     
+    """
+
+SCRIPT = """ 
+    <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>      
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+
+    <script>
+    $(document).ready(function() {
+        $('#VF_table').DataTable( {
+            dom: 'Bfrtip',
+            "pageLength": 20,
+            "searching": true,
+            "bLengthChange": false,
+            "paging":   true,
+            "info": false
+        } );
+    } );
+    </script>
+    """
+
+HTML = '''
+<!DOCTYPE html>
+<html>
+    <head>  
+    %s
+    %s
+    </head>  
+
+    <body>
+    <div id='vftable' style="max-width:90%%; padding-left:35px;">
+        <h1>Virulence Factors of sample: %s</h1>
+        %s
+    </div>    
+    </body>
+<html>
+'''
+
+def parse_VF_annotation(vfdb_annotation):
+    vf_accession2annotation = {}
+    with open(vfdb_annotation, 'r') as f:
+        for row in f:
+            data = row.split('\t')
+            vf_accession2annotation[data[0]] = data[1:]
+    return vf_accession2annotation
+
+def prepare_dataframe(blast_result, vf_accession2annotation):
+    vf_gene_template = '<a href="http://www.mgc.ac.cn/cgi-bin/VFs/gene.cgi?GeneID=%s">%s</a>'
+    vf_template = '<a href=http://www.mgc.ac.cn/cgi-bin/VFs/vfs.cgi?VFID=%s#%s>%s</a> (%s)'
+    VFDB_link = '<a href=https://www.vfdb.chlamdb.ch/VF_VFDB/%s>Link</a>'
+
+    blast_result_table = pandas.read_csv(blast_result, delimiter="\t", header=0, index_col=0)
+
+    vf_data = []
+    header = ['ORF ID', "Hit", "Virulence Factor", "Link Conservation", "Product", "Organism", "Identity (%)", "e-value", "Bitscore"]
+    for protein_id, blast_row in blast_result_table.iterrows():
+        vf_accession  = blast_row["matching_sequence"]
+        percentage_identity =  blast_row["percentage_identity"]
+        bitscore =  blast_row["bitscore"]
+        evalue = blast_row["e-value"]
+
+        annotation = vf_accession2annotation[vf_accession]
+        vf_id = annotation[0]
+        vf_description = annotation[3]
+        product = annotation[4]
+        bacteria =annotation[2]
+
+        vf_data.append([protein_id,
+                        vf_gene_template % (vf_accession, vf_accession),
+                        vf_template % (vf_id, vf_id,vf_id, vf_description),
+                        VFDB_link % vf_accession,
+                        product,
+                        bacteria,
+                        percentage_identity,
+                        evalue,
+                        bitscore])
+
+    df = pandas.DataFrame(vf_data, columns=header)
+    # cell content is truncated if colwidth not set to -1
+    pandas.set_option('display.max_colwidth', -1)
+
+    df_str = df.to_html(
+        index=False,
+        bold_rows=False,
+        classes=["display", "dataTable"],
+        table_id="VF_table",
+        escape=False,
+        border=0)
+    return df_str
+
+vf_accession2annotation = parse_VF_annotation(vfdb_annotation)
 
 with open(report_file, 'w') as f:
-    f.write(template % (sample,
-                        table_rows))
+    f.write(HTML % (SCRIPT,
+                    STYLE,
+                    sample,
+                    prepare_dataframe(blast_result,
+                                      vf_accession2annotation)))
