9f8b457990c2895bb47cd29ace46bdbe561609aa
Trestan Pillonel <trestan.pillonel@gmail.com>
2018-11-27 18:34:45 +0100

    generate html snp tables from individual filtered vcf files

--- rules/report_generation/scripts/snps_reports.py
+++ rules/report_generation/scripts/snps_reports.py
@@ -2,15 +2,22 @@
 import pandas
 import re
 import numpy
+import vcf
+from Bio import SeqIO
 
 # inputs
 vcf_file = snakemake.input["vcf_file"]
 merged_vcf = snakemake.input["merged_vcf"]
-gbk_file = snakemake.input["merged_vcf"]
+gbk_file = snakemake.input["gbk_file"]
+reference = snakemake.params["reference"]
+if "_assembled_genome" in reference:
+    reference = re.sub("_assembled_genome", "", reference)
+    assembleg_genome = True
 
 # output
-report_file = snakemake.output[0]
+report_file = snakemake.output["html_file"]
 
+merged_vcf_records = [i for i in vcf.Reader(open(merged_vcf, 'r'))]
 
 def make_div(figure_or_data,
              include_plotlyjs=False,
@@ -38,13 +45,21 @@ def make_div(figure_or_data,
 
 
 def parse_gbk(gbk_file):
-
+    print("gb", gbk_file)
     record_dict = SeqIO.to_dict(SeqIO.parse(gbk_file, 'genbank'))
+    print("record_dict", record_dict)
+
     return record_dict
 
 
 def get_neiboring_orf(position, feature_list):
-
+    '''
+    Identify neiboring feature of variant position.
+    Input: SeqRecord and position (integer)
+    Output: List of two strings with the closest features located before and after the input position
+    (either locus_tags/gene names).
+    If no feature found (eg. in the case where the locate in close to the begining/end of the record), return "no CDS".
+    '''
     if int(position) > int(feature_list[-1].location.end):
         try:
             gene = feature_list[-1].qualifiers["gene"][0]
@@ -68,7 +83,6 @@ def get_neiboring_orf(position, feature_list):
         if feature.type != 'source':
             first_feature = True
 
-
         if int(position) > int(feature.location.end) and int(position) < int(feature_list[n + 1].location.start):
             try:
                 gene1 = feature.qualifiers["gene"][0]
@@ -86,7 +100,39 @@ def get_neiboring_orf(position, feature_list):
     return ["no CDS", "no CDS"]
 
 
+def check_reference_mapping_GT(vcf_record_list,
+                               contig,
+                               position,
+                               sampe_reference):
+    '''
+    Quality check. Check if a variant position is also a variant in the mapping
+    the mapping of the reference reads versus the reference assembly.
+    Only necessary when dealing with assembled references.
+    Input: merged VCF file with all variant positions (all_samples_snp.vcf).
+    Return False if a variant is found at the given position.
+    '''
+    print("vcf check--------------")
+    print(contig, position, sampe_reference)
+    for record in vcf_record_list:
+        if record.CHROM == contig and record.POS == position:
+            for sample in record.samples:
+                if sample.sample == sampe_reference:
+                    if len(record.FILTER) == 0:
+                        PASS = "PASS"
+                    else:
+                        PASS = "FAIL"
+
+                    if sample['GT'] not in ['.', 0]:
+                        return [True, PASS]
+                    else:
+                        return [False, PASS]
+    return [None, None]
+
 def parse_vcf(vcf_file, gbk_file):
+    '''
+    Given a vcf input file and the gbk of the reference genome, return an html table of identified variants.
+    '''
+
     from copy import copy
     from Bio.Seq import Seq
     from Bio.SeqRecord import SeqRecord
@@ -97,14 +143,19 @@ def parse_vcf(vcf_file, gbk_file):
 
     vcf_reader = vcf.Reader(open(vcf_file, 'r'))
     gbk_dico = parse_gbk(gbk_file)
-
+    print(gbk_dico.keys())
     intergenic = 0
     synonymous = 0
     non_synonymous = 0
     indel = 0
 
-    filter_head = ['%s (%s)' % (vcf_reader.filters[i].id, vcf_reader.filters[i].desc) for i in vcf_reader.filters ]
-    header = ["contig", "contig_length", "position", "AC", "REF", "ALT", "MUTATION_location", "MUTATION_type", "ORF", "gene", "orf_before", "orf_after"] + filter_head
+    filter_head = ['%s' % (vcf_reader.filters[i].id) for i in vcf_reader.filters]
+    header = ["contig", "length", "position", "REF", "ALT", "location", "type", "ORF", "gene", "orf_before", "orf_after"]
+    header += filter_head
+    if 'assembled' in vcf_file:
+        header.append("InRef")
+        header.append("Fail Others")
+
     table_rows = []
     for n, record in enumerate(vcf_reader):
 
@@ -128,20 +179,20 @@ def parse_vcf(vcf_file, gbk_file):
 
                     if len(record.ALT[0]) > 1:
                         mut_type = 'INDEL'
-                        indel+=1
+                        indel += 1
                         continue
                     else:
                         aa_seq_ref = str(feature.extract(record_alt.seq).translate())
                         # mutate reference sequence
-                        record_alt.seq[int(record.POS)-1] = str(record.ALT[0])
+                        record_alt.seq[int(record.POS) - 1] = str(record.ALT[0])
                         # check if synonymous or not
                         aa_seq_alt = str(feature.extract(record_alt.seq).translate())
 
                         if str(aa_seq_ref) == str(aa_seq_alt):
-                            mut_type = 'SYNONYMOUS'
+                            mut_type = 'S'
                             synonymous += 1
                         else:
-                            mut_type = 'NON SYNONYMOUS'
+                            mut_type = 'NS'
                             if len(record.FILTER) == 0:
                                 non_synonymous+=1
                         #print (str(aa_seq_ref.translate()) == str(record_alt.translate()))
@@ -152,10 +203,23 @@ def parse_vcf(vcf_file, gbk_file):
         # print (filter_dico["PASS"].id)
         # print (filter_dico["PASS"].desc)
         contig = record.CHROM
-        ac = record.INFO["AC"][0]
+        print("depth", record.samples[0]['AD'], record.samples[0]['DP'])
+        if record.samples[0]['GT'] in ['.', 0]:
+            # print(type(ac))
+            # if ac == 0:
+            continue
         position = record.POS
-        ref = record.REF
-        alt = record.ALT[0]
+        ref = "%s (%s/%s)" % (record.REF,
+                              record.samples[0]['AD'][0],
+                              record.samples[0]['DP'])
+        if len(record.ALT[0]) == 1:
+            alt = "%s (%s/%s)" % (record.ALT[0],
+                                  record.samples[0]['AD'][1],
+                                  record.samples[0]['DP'])
+        else:
+            alt = "%sbp (%s/%s)" % (len(record.ALT[0]),
+                                    record.samples[0]['AD'][1],
+                                    record.samples[0]['DP'])
         filter_status = []
 
         # if any of the test failed, set PASS as failed
@@ -167,26 +231,36 @@ def parse_vcf(vcf_file, gbk_file):
                 if filter_name == 'PASS':
                     filter_status.append('NO')
                 else:
-                    filter_status.append('FAIL')
+                    filter_status.append('-')
             else:
                 if filter_name == 'PASS':
                     filter_status.append('YES')
                 else:
-                    filter_status.append('SUCCESS')
+                    filter_status.append('+')
+        row = [contig,
+               len(record_alt),
+               position,
+               ref,
+               alt,
+               mut_location,
+               mut_type,
+               orf_name,
+               gene,
+               orf_before,
+               orf_after]
 
-        table_rows.append("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (contig,
-                                                                                  len(record_alt),
-                                                                                  position,
-                                                                                  ac,
-                                                                                  ref,
-                                                                                  alt,
-                                                                                  mut_location,
-                                                                                  mut_type,
-                                                                                  orf_name,
-                                                                                  gene,
-                                                                                  orf_before,
-                                                                                  orf_after,
-                                                                                  '\t'.join(list(filter_status))))
+        row += list(filter_status)
+
+        if 'assembled' in vcf_file:
+            GT, PASS = check_reference_mapping_GT(merged_vcf_records,
+                                            contig,
+                                            position,
+                                            reference)
+            print("################# GT #################")
+            row.append(GT)
+            row.append(PASS)
+
+        table_rows.append(row)
 
     df = pandas.DataFrame(table_rows, columns=header)
 
@@ -208,7 +282,15 @@ STYLE = """
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css"/>
     <style type="text/css">
-    body{font-family:Helvetica,arial,sans-serif;font-size:14px;line-height:1.6;padding-bottom:10px;background-color:#fff;color:#333;margin:0}body>div .section::before{content:"";display:block;height:80px;margin:-80px 0 0}#bubble-plot::before{margin:0}.topic-title{font-size:18pt}body>div>.section{margin-left:255px;margin-bottom:3em}div.section{margin-right:20px}#contents>p{display:none}button,li p.first{display:inline-block}#contents{margin-top:80px;padding-left:0;width:235px;background-color:#f1f1f1;height:100%;position:fixed;overflow:auto}#contents ul{list-style-type:none}#contents ul>li{font-size:14pt}#contents ul>li a:hover{color:#151d26}button,h1.title{color:#fff;background-color:#151d26}#contents ul>li>ul>li{font-size:12pt}h1.title{margin-top:0;position:fixed;z-index:10;padding:20px;width:100%}code,table tr:nth-child(2n),tt{background-color:#f8f8f8}.one-col{min-width:310px;height:500px;margin:0 auto}.two-col-left{height:300px;width:49%;float:left}.two-col-right{height:300px;width:49%;float:right}button{margin:0 5px 0 0;padding:5px 25px;font-size:18px;line-height:1.8;appearance:none;box-shadow:none;border-radius:3px;border:none}button:focus{outline:0}button:hover{background-color:#4183C4}button:active{background-color:#27496d}.legend-rect{width:20px;height:20px;margin-right:8px;margin-left:20px;float:left;-webkit-border-radius:2px;border-radius:2px}a{color:#4183C4;text-decoration:none}a.absent{color:#c00}a.anchor{padding-left:30px;margin-left:-30px;cursor:pointer;position:absolute;top:0;left:0;bottom:0}dl,dl dt,dl dt:first-child,hr,table,table tr{padding:0}table tr td,table tr th{border:1px solid #ccc;text-align:left;padding:6px 13px}h1,h2,h3,h4,h5,h6{margin:20px 0 10px;padding:0;font-weight:700;-webkit-font-smoothing:antialiased;cursor:text;position:relative}h1:hover a.anchor,h2:hover a.anchor,h3:hover a.anchor,h4:hover a.anchor,h5:hover a.anchor,h6:hover a.anchor{text-decoration:none}h1 code,h1 tt,h2 code,h2 tt,h3 code,h3 tt,h4 code,h4 tt,h5 code,h5 tt,h6 code,h6 tt{font-size:inherit}h1{font-size:28px;color:#151d26;border-bottom:1px solid #ccc}h2{font-size:24px;color:#000}h3{font-size:18px}h4{font-size:16px}dl dt,h5,h6{font-size:14px}h6{color:#777}blockquote,dl,li,ol,p,pre,table,ul{margin:15px 0}hr{background:url(http://tinyurl.com/bq5kskr) repeat-x;border:0;color:#ccc;height:4px}a:first-child h1,a:first-child h2,a:first-child h3,a:first-child h4,a:first-child h5,a:first-child h6{margin-top:0;padding-top:0}h1 p,h2 p,h3 p,h4 p,h5 p,h6 p{margin-top:0}dl dt{font-weight:700;font-style:italic;margin:15px 0 5px}blockquote>:first-child,dl dd>:first-child,dl dt>:first-child,table tr td :first-child,table tr th :first-child{margin-top:0}blockquote>:last-child,dl dd>:last-child,dl dt>:last-child{margin-bottom:0}dl dd{margin:0 0 15px;padding:0 15px}blockquote{border-left:4px solid #ddd;padding:0 15px;color:#777}table{border-spacing:0;border-collapse:collapse}table tr{border-top:1px solid #ccc;background-color:#fff;margin:0}table tr th{font-weight:700;margin:0}table tr td{margin:0}table tr td :last-child,table tr th :last-child{margin-bottom:0}img{max-width:100%}span.frame{display:block;overflow:hidden}span.frame>span{border:1px solid #ddd;display:block;float:left;overflow:hidden;margin:13px 0 0;padding:7px;width:auto}span.frame span img{display:block;float:left}span.frame span span{clear:both;color:#333;display:block;padding:5px 0 0}span.align-center{display:block;overflow:hidden;clear:both}span.align-center>span{display:block;overflow:hidden;margin:13px auto 0;text-align:center}span.align-center span img{margin:0 auto;text-align:center}span.align-right{display:block;overflow:hidden;clear:both}span.align-right>span{display:block;overflow:hidden;margin:13px 0 0;text-align:right}span.align-right span img{margin:0;text-align:right}span.float-left{display:block;margin-right:13px;overflow:hidden;float:left}span.float-left span{margin:13px 0 0}span.float-right{display:block;margin-left:13px;overflow:hidden;float:right}span.float-right>span{display:block;overflow:hidden;margin:13px auto 0;text-align:right}code,tt{margin:0 2px;padding:0 5px;white-space:nowrap;border:1px solid #eaeaea;border-radius:3px}pre code{margin:0;padding:0;white-space:pre;background:0 0}.highlight pre,pre{background-color:#f8f8f8;border:1px solid #ccc;font-size:13px;line-height:19px;overflow:auto;padding:6px 10px;border-radius:3px}pre code,pre tt{background-color:transparent;border:none}div#metadata{text-align:right}h1{line-height:1.6}.simple{padding-left:20px}.docutils.container{width:100%}
+    .docutils.container{width:100%}
+    #snps_table {
+        style="width:100%";
+        word-break: normal;
+    }
+    body{
+      margin: 20px;
+      padding: 20px;
+    }
     .pull-left{
     .dataTables_filter {
        float: left !important;
@@ -233,19 +315,37 @@ SCRIPT = """
     <script>
 
     $(document).ready(function() {
-        $('#res_table').DataTable( {
+        $('#snps_table').DataTable( {
             dom: 'Bfrtip',
-            "pageLength": 20,
+            "pageLength": 50,
             "searching": true,
             "bLengthChange": false,
             "paging":   true,
             "info": false,
             'rowCallback': function(row, data, index){
-            if(data[6] < 90){
-                             $(row).find('td:eq(6)').css('background-color', 'rgba(255, 0, 0, 0.6)');
+            if(data[16] == "-" ){
+                             $(row).find('td:eq(16)').css('background-color', 'rgba(255, 0, 0, 0.6)');
                             }
-            if(data[5] < 90){
-                             $(row).find('td:eq(5)').css('background-color', 'rgba(0, 255, 0, 0.6)');
+            if(data[15] == "-" ){
+                             $(row).find('td:eq(15)').css('background-color', 'rgba(255, 0, 0, 0.6)');
+                            }
+            if(data[14] == "-" ){
+                             $(row).find('td:eq(14)').css('background-color', 'rgba(255, 0, 0, 0.6)');
+                            }
+            if(data[13] == "-" ){
+                             $(row).find('td:eq(13)').css('background-color', 'rgba(255, 0, 0, 0.6)');
+                            }
+            if(data[11] == "YES"){
+                             $(row).find('td:eq(11)').css('background-color', 'rgba(0, 255, 0, 0.6)');
+                            }
+            if(data[17] == "FAIL"){
+                             $(row).find('td:eq(17)').css('background-color', 'rgba(255, 0, 0, 0.6)');
+                            }
+            if(data[17] == "PASS"){
+                             $(row).find('td:eq(17)').css('background-color', 'rgba(0, 255, 0, 0.6)');
+                            }
+            if(data[16] == "True"){
+                             $(row).find('td:eq(16)').css('background-color', 'rgba(255, 0, 0, 0.6)');
                             }
             },
         } );
@@ -274,7 +374,7 @@ def write_report(output_file,
     {STYLE}
 
 =============================================================
-RGI report
+SNPS report
 =============================================================
 
 .. contents::
