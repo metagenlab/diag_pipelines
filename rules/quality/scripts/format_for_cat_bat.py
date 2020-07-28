6b8e363e974e271cc32439a5c7cf624cc5e27e2b
tpillone <trestan.pillonel@gmail.com>
2020-07-28 10:15:59 +0200

    added checkm rules

---
+++ rules/quality/scripts/format_for_cat_bat.py
@@ -0,0 +1,8 @@
+
+
+# checkm header fromat
+# >N1372 contig_1 geneId=1;start=516;end=1682;strand=1;protlen=389 marker=PF06965.7;mstart=5;mend=380
+
+# needed header formats
+# N1372.fna_contig_1_1
+
